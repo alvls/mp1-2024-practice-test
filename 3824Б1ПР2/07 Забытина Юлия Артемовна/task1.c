b894153858f54fa06d3b1c08fe0018a92b9c1605
Юлия <uzabytina9@gmail.com>
2024-12-19 09:55:47 +0300

    Update task1.c

--- 3824Б1ПР2/07 Забытина Юлия Артемовна/task1.c
+++ 3824Б1ПР2/07 Забытина Юлия Артемовна/task1.c
@@ -1,7 +1,27 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между первыми
 // двумя нулями.
-int task1(int A[], size_t n)
-{
-  return -1;
+int task1(int A[], size_t n){
+  int is_null_first = 0;
+  int is_null_second = 0;
+  
+  int first_null_is_found = 0;
+  int second_null_is_found = 0;
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+	if (A[i] == 0 && first_null_is_found == 1 && second_null_is_found == 0) {
+		is_null_second = i;
+		second_null_is_found = 1;
+	}
+	if (A[i] == 0 && first_null_is_found == 0 && second_null_is_found == 0) {
+		is_null_first = i;
+		first_null_is_found = 1;
+	}
+}
+  
+for (int j = is_null_first + 1; j < is_null_second; j++) {
+	sum += A[j];
+}
+return sum;
 }

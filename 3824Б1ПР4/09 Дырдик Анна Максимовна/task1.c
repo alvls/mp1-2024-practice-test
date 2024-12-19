0584ae43d33ec76af5c2e62bb5b53c6d06435220
Анна Дырдик <anadyrdik@gmail.com>
2024-12-19 09:53:17 +0300

    Update task1.c

--- 3824Б1ПР4/09 Дырдик Анна Максимовна/task1.c
+++ 3824Б1ПР4/09 Дырдик Анна Максимовна/task1.c
@@ -1,6 +1,19 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
+#include <stdio.h>
 int task1(int A[], size_t n)
 {
-  return -1;
+    if (n == 0) {
+        return -1;
+    }
+
+    size_t min_index = 0;
+  
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] <= A[min_index]) {
+            min_index = i;
+        }
+    }
+
+    return (int)min_index;
 }

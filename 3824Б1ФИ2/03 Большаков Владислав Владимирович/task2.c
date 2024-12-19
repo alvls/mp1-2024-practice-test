7ed12deb4ffc4f3bd36842d6463b577dfd5e585a
vladB77-name <vladislav.student.08@mail.ru>
2024-12-19 10:39:28 +0300

    Update task2.c

--- 3824Б1ФИ2/03 Большаков Владислав Владимирович/task2.c
+++ 3824Б1ФИ2/03 Большаков Владислав Владимирович/task2.c
@@ -1,6 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdio.h>
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
 size_t task2(double A[], size_t n)
 {
-  return 0;
+	size_t count = 0; 
+	for (size_t i = 0; i < n; i++) {
+		int flag = 0;
+		for (size_t j = 0; j < i; j++) {
+			if (fabs(A[j] - A[i]) < 1e-10) {
+				flag = 0;
+				break;
+			}
+			else {
+				flag = 1;
+			}
+		}
+		if (flag == 0) {
+			count++;
+		}
+	}
+	size_t res = n - count;
+	return res;
 }

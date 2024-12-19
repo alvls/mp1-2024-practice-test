5433c0900709e4c679487a90552629c488ffdadc
rat-noob <romakapranov16@gmail.com>
2024-12-19 09:23:20 +0300

    Update task1.c

--- 3824Б1ПР1/09 Капранов Роман Валерьевич/task1.c
+++ 3824Б1ПР1/09 Капранов Роман Валерьевич/task1.c
@@ -2,5 +2,17 @@
 // Найти номер его последнего минимального элемента.
 int task1(int A[], size_t n)
 {
-  return -1;
+  int min;
+  int mini;
+  min = A[0];
+  mini = 0;
+  for (int i = 0; i < n; i++)
+  {
+	  if (A[i] <= min)
+	  {
+		  min = A[i];
+		  mini = i;
+	  }
+  }
+  return mini;
 }

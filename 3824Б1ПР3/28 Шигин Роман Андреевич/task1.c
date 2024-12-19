fb806d35b69529367f14b94f9e1f099c21a8dd94
Rigtsu <siginroma1@gmail.com>
2024-12-19 10:08:45 +0300

    Update task1.c

--- 3824Б1ПР3/28 Шигин Роман Андреевич/task1.c
+++ 3824Б1ПР3/28 Шигин Роман Андреевич/task1.c
@@ -3,5 +3,22 @@
 // двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1111111111;
+  int last_zero_index = -1;
+  int second_last_zero_index = -1;
+  for (int i = n - 1; i >= 0; i--) {
+      if (A[i] == 0) {
+          if (last_zero_index == -1) {
+              last_zero_index = i;
+          }
+          else if (second_last_zero_index == -1) {
+              second_last_zero_index = i;
+              break;
+          }
+      }
+  }
+  int sum = 0;
+  for (int i = second_last_zero_index + 1; i < last_zero_index; i++) {
+      sum += A[i];
+  }
+  return sum;
 }

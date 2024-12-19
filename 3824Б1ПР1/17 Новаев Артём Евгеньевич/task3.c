516a0b4a3a3abb64ffdf140e3f73ecfbbc413bd0
monster-programming <artemnovaev9@gmail.com>
2024-12-19 09:43:35 +0300

    Update task3.c

--- 3824Б1ПР1/17 Новаев Артём Евгеньевич/task3.c
+++ 3824Б1ПР1/17 Новаев Артём Евгеньевич/task3.c
@@ -1,7 +1,35 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить количество слов, 
 // которые начинаются и заканчиваются одной и той же буквой. 
+int func(char a, char b) {
+    if (a >= 'A' && a <= 'Z') {
+        a += ('a' - 'A');
+    }
+    if (b >= 'A' && b <= 'Z') {
+        b += ('a' - 'A');
+    }
+    return a == b;
+}
+
 int task3(char *s)
 {
-  return -1;
+  int count = 0;
+  int len = strlen(s);
+  char word[100];
+  int wordIndex = 0;
+
+  for (int i = 0; i <= len; i++) {
+    if (s[i] != ' ' && s[i] != '\0') {
+        word[wordIndex++] = s[i];
+    }
+    else if (wordIndex > 0) {
+        word[wordIndex] = '\0';
+        if (func(word[0], word[wordIndex - 1])) {
+            count++;
+        }
+        wordIndex = 0;
+      }
+  }
+
+  return count;
 }

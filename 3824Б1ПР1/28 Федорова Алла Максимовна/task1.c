e23e1a047e9b71ed97d80d5b20fb9e64e749abf8
FedorovaAlla <fedorova.alla0616@gmail.com>
2024-12-19 09:12:09 +0300

    Update task1.c

--- 3824Б1ПР1/28 Федорова Алла Максимовна/task1.c
+++ 3824Б1ПР1/28 Федорова Алла Максимовна/task1.c
@@ -2,6 +2,29 @@
 // не менее двух нулей. Найти сумму элементов, расположенных между последними 
 // двумя нулями.
 int task1(int A[], size_t n)
-{
-  return -1111111111;
+    int lastZeroIndex = -1;
+    int secondLastZeroIndex = -1;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (A[i] == 0) {
+            if (lastZeroIndex == -1) {
+                lastZeroIndex = i;
+            }
+            else {
+                secondLastZeroIndex = i;
+                break;
+            }
+        }
+    }
+
+    if (secondLastZeroIndex == -1) {
+        return -1111111111;
+    }
+
+    int sum = 0;
+    for (int i = secondLastZeroIndex + 1; i < lastZeroIndex; i++) {
+        sum += A[i];
+    }
+
+    return sum;
 }

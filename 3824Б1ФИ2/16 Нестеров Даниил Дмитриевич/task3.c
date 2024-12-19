6c70d48c47eb5f81d48635516a8fdb34947f0519
KofTD <85734003+KofTD@users.noreply.github.com>
2024-12-19 10:39:12 +0300

    Update task3.c

--- 3824Б1ФИ2/16 Нестеров Даниил Дмитриевич/task3.c
+++ 3824Б1ФИ2/16 Нестеров Даниил Дмитриевич/task3.c
@@ -1,6 +1,24 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить количество слов в строке.
-int task3(char *s)
-{
-  return -1;
+int task3(char *s) {
+  size_t len       = strlen(s);
+  bool was_in_word = false;
+
+  int word_counter = 0;
+  for (size_t i = 0; i < len; i++) {
+    if (s[i] == ' ') {
+      if (was_in_word) { word_counter++; }
+      was_in_word = false;
+    } else {
+      was_in_word = true;
+    }
+  }
+
+  if (was_in_word) { word_counter++; }
+
+
+  return word_counter;
 }

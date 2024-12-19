69f20e13fecda1ad8d97fd1223b9370e00057e9c
DontelCardem54 <93559646+DontelCardem54@users.noreply.github.com>
2024-12-19 09:56:52 +0300

    Update task2.c

--- 3824Б1ПР2/27 Черных Валентин Андреевич/task2.c
+++ 3824Б1ПР2/27 Черных Валентин Андреевич/task2.c
@@ -1,8 +1,22 @@
+#include <stdlib.h>
+
 // Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
 // Найти значение минимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+    long long result = LLONG_MAX;
+
+    for (size_t i = 1; i < n - 1; i++) {
+        if (A[i - 1] >= A[i] && A[i + 1] >= A[i]) {
+            result = A[i] < result ? A[i] : result;
+        }
+    }
+
+    if (result == LLONG_MAX) {
+        return -1;
+    }
+
+    return result;
 }

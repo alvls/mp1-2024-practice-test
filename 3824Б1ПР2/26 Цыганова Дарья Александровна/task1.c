6074bb2a5f4b5042c8c042f40ec5b124bdb1ce66
dariatsyganovaa <dcyganova857@gmail.com>
2024-12-19 10:02:08 +0300

    Update task1.c

--- 3824Б1ПР2/26 Цыганова Дарья Александровна/task1.c
+++ 3824Б1ПР2/26 Цыганова Дарья Александровна/task1.c
@@ -1,6 +1,7 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего правого соседа.
 #include <stddef.h>
+#include <stdlib.h>
 size_t task1(int A[], size_t n) {
     size_t count = 0;
     for (size_t i = 0; i < n - 1; i++) {

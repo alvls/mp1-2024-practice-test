630441de28a35bbf4f740242f91cb6d89593837f
KofTD <85734003+KofTD@users.noreply.github.com>
2024-12-19 10:38:28 +0300

    Update task1.c

--- 3824Б1ФИ2/16 Нестеров Даниил Дмитриевич/task1.c
+++ 3824Б1ФИ2/16 Нестеров Даниил Дмитриевич/task1.c
@@ -1,7 +1,55 @@
+#include <limits.h>
+#include <stdlib.h>
 // Задача 1. Дан непустой массив A из N элементов.
 // Найти количество элементов, расположенных между первым максимальным
 // и последним минимальным элементами.
-int task1(unsigned A[], size_t N)
-{
-  return -1;
+
+size_t find_max(const unsigned* arr, size_t size) {
+  size_t index     = ULONG_MAX;
+  unsigned int val = 0;
+
+  for (size_t i = 0; i < size; i++) {
+    if (arr[i] > val) {
+      index = i;
+      val   = arr[i];
+    }
+  }
+
+  return index;
+}
+
+size_t find_min(const unsigned* arr, size_t size) {
+  size_t index     = ULONG_MAX;
+  unsigned int val = INT_MAX;
+
+  for (size_t i = 0; i < size; i++) {
+    if (arr[i] <= val) {
+      index = i;
+      val   = arr[i];
+    }
+  }
+
+  return index;
+}
+
+int task1(unsigned A[], size_t N) {
+  size_t max_ind = find_max(A, N);
+  size_t min_ind = find_min(A, N);
+
+  size_t start = 0;
+  size_t end   = ULONG_MAX;
+
+  if (max_ind < min_ind) {
+    start = max_ind;
+    end   = min_ind;
+  } else {
+    start = min_ind;
+    end   = max_ind;
+  }
+
+  int counter = 0;
+  for (size_t i = start + 1; i < end; i++) { counter++; }
+
+
+  return counter;
 }

ddfd430f16c3fcc496fe2ffd7b92a2baffd6187e
MaksimBorisovFI3 <morgenhto@gmail.com>
2024-12-19 09:43:15 +0300

    Update task2.c

--- 3824Б1ФИ3/05 Борисов Максим Викторович/task2.c
+++ 3824Б1ФИ3/05 Борисов Максим Викторович/task2.c
@@ -3,5 +3,20 @@
 // Локальный максимум – элемент, который >= левого и правого соседей.
 size_t task2(double A[], size_t n)
 {
-  return -1;
+  if (n == 1) {
+      return 1; 
+  }
+  size_t count = 0; 
+  for (size_t i = 0; i < n; i++) {
+      if (i == 0 && A[i] >= A[i + 1]) {
+          count++;
+      }
+      else if (i == n - 1 && A[i] >= A[i - 1]) {
+          count++;
+      }
+      else if (i > 0 && i < n - 1 && A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+          count++;
+      }
+  }
+  return count;
 }

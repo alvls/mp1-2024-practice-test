5e4a549b080ae2ecbfc239ca9f06fd23b44b3113
Rigtsu <siginroma1@gmail.com>
2024-12-19 10:34:02 +0300

    Update task3.c

--- 3824Б1ПР3/28 Шигин Роман Андреевич/task3.c
+++ 3824Б1ПР3/28 Шигин Роман Андреевич/task3.c
@@ -3,5 +3,22 @@
 // Пример: s = "aBc1D2e34F56g", ответ: "aceg".
 char* task3(char *s)
 {
-  return "task3";
+    size_t length = 0;
+    for (const char* p = s; *p != '\0'; ++p) {
+        if (islower(*p)) {
+            length++;
+        }
+    }
+
+    char* result = (char*)malloc((length + 1) * sizeof(char));
+
+    char* dst = result;
+    for (const char* p = s; *p != '\0'; ++p) {
+        if (islower(*p)) {
+            *dst++ = *p;
+        }
+    }
+    *dst = '\0';
+
+    return result;
 }

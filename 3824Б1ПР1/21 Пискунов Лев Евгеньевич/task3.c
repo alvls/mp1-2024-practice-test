0ac5360aede02ff3468a02c402dbf3f8c9c5245f
LCDIIL <100310697+LCDIIL@users.noreply.github.com>
2024-12-19 10:21:23 +0300

    Update task3.c

--- 3824Б1ПР1/21 Пискунов Лев Евгеньевич/task3.c
+++ 3824Б1ПР1/21 Пискунов Лев Евгеньевич/task3.c
@@ -1,6 +1,16 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
-char* task3(char *s)
-{
-  return "task3";
+char* task3(char* s) {
+
+ int li = -1, k = 0;
+ for (int i = 0; s[i] != '\0'; i++) li++;
+ char* res = (char*)malloc(sizeof(char) * (li + 2));
+ for (int i = li; i > -1; i--) {
+  res[k] = s[i];
+  k++;
+ }
+ res[li + 1] = '\0';
+ for (int i = 0; res[i] != '\0'; i++) s[i] = res[i];
+ free(res);
+ return s;
 }

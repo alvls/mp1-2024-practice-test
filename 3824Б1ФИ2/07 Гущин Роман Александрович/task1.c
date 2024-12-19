80901b223d69290101d52fc42accd2ff2ccff3b6
romik3211 <149517130+romik3211@users.noreply.github.com>
2024-12-19 09:26:17 +0300

    Update task1.c

--- 3824Б1ФИ2/07 Гущин Роман Александрович/task1.c
+++ 3824Б1ФИ2/07 Гущин Роман Александрович/task1.c
@@ -1,7 +1,24 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между первыми
 // двумя нулями.
+
 int task1(int A[], size_t n)
 {
-  return -1;
+  int sum = 0, first = -1, second = -1;
+  for (int i = 0; i < n; i++) {
+    if (A[i] == 0) {
+      if (first == -1) 
+        first = i;
+      else {
+        second = i;
+        break;
+      }
+    }
+  }
+  if (first != -1 && second != -1) {
+    for (int i = first + 1; i < second; i++) {
+      sum += A[i];
+    }
+  }
+  return sum;
 }

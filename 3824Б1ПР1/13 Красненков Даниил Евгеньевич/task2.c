c3536354f37e7e35d7d277c41449746b9877b8b6
DaniilKrasnenkov81 <krasnenkovdaniil1@gmail.com>
2024-12-19 10:38:30 +0300

    Update task2.c

--- 3824Б1ПР1/13 Красненков Даниил Евгеньевич/task2.c
+++ 3824Б1ПР1/13 Красненков Даниил Евгеньевич/task2.c
@@ -4,5 +4,23 @@
 // Найти длину наибольшей серии.
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+     size_t max_length = 1;
+    size_t length = 1;
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] == A[i - 1]) {
+            length++;
+        }
+        else {
+            if (length > max_length) {
+                max_length = length;
+            }
+            length = 1;
+        }
+    }
+
+    if (length > max_length) {
+        max_length = length;
+    }
+
+    return max_length;
 }

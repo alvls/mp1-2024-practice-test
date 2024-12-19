6e139658e621b76dddb69b4487915e1a7d7e1a6c
NititaTch <136841289+MNikitattt@users.noreply.github.com>
2024-12-19 09:17:12 +0300

    Update task1.c

--- 3824Б1ФИ2/30 Щербаков Никита Андреевич/task1.c
+++ 3824Б1ФИ2/30 Щербаков Никита Андреевич/task1.c
@@ -2,5 +2,14 @@
 // Найти номер его последнего максимального элемента.
 int task1(int A[], size_t n)
 {
-  return -1;
+  int element = 0;
+	for (size_t i = 0; i < n - 1; i++)
+	{
+		if (A[i] < A[i + 1])
+		{
+			element = i + 1;
+		}
+		printf("%zu\n", i);
+	}
+	return element + 1;
 }

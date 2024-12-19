6b6cc677922f34a068331e2efab081d83ebdfc3f
Neqie <112955126+Neqie@users.noreply.github.com>
2024-12-19 10:03:16 +0300

    Update task3.c

--- 3824Б1ФИ3/26 Тихановский Антон Михайлович/task3.c
+++ 3824Б1ФИ3/26 Тихановский Антон Михайлович/task3.c
@@ -1,7 +1,20 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из цифр,
 // содержащихся в исходной строке s.
 // Пример: s = "abc1d2e34f56g", ответ: "123456".
-char* task3(char *s)
-{
-  return "task3";
+#include <stdio.h>
+#include <string.h>
+#define MAX_LENGTH 10000
+
+char* task3(char *s) {
+  char result[MAX_LENGTH];
+  int j = 0; 
+  for (int i = 0; s[i] != '\0'; i++) {
+      if (s[i] >= '0' && s[i] <= '9') {
+          if (j < MAX_LENGTH - 1) { 
+              result[j++] = s[i];
+          }
+      }
+  }
+  result[j] = '\0'; 
+  return result;
 }

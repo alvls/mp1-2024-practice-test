ac11abf0ee294b4ff0c785a531d2406831266dab
Dmitry <86683621+DmHack@users.noreply.github.com>
2024-12-19 09:17:42 +0300

    Update task1.c

--- 3824Б1ПР3/12 Нарюков Дмитрий Алексеевич/task1.c
+++ 3824Б1ПР3/12 Нарюков Дмитрий Алексеевич/task1.c
@@ -3,5 +3,18 @@
 // Вернуть 0, если требуемые элементы отсутствуют.
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+    unsigned maxCh = 0;
+    int flagInt = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        if (A[i] > a && A[i] < b) {
+            if (!flagInt || A[i] > maxCh) {
+                maxCh = A[i];
+                flagInt = 1;
+            }
+        }
+    }
+
+    if (flagInt) return maxCh;
+    else return 0;
 }

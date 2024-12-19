8b38f8e11a71efc00f6151cd85f2da11b4063236
Zetix3 <149903857+Zetix3@users.noreply.github.com>
2024-12-19 09:23:03 +0300

    Update task1.c

--- 3824Б1ФИ3/12 Зиновьев Тимофей Сергеевич/task1.c
+++ 3824Б1ФИ3/12 Зиновьев Тимофей Сергеевич/task1.c
@@ -3,5 +3,14 @@
 // Вернуть 0, если требуемые элементы отсутствуют.
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+	int max = 0;
+	
+	for (int i = 0; i < n; i++) {
+		if ((A[i] > a) && (A[i] < b)) {
+			if (A[i] > max) {
+				max = A[i];
+			}
+		}
+	}
+	return max;
 }

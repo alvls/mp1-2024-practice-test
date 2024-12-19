65530b2c57024f0d72c248f8773682bed2cbcf1a
Omenvl <alebzzzzzz@gmail.com>
2024-12-19 09:57:40 +0300

    Update task1.c

--- 3824Б1ПР3/16 Пьянзин Илья Андреевич/task1.c
+++ 3824Б1ПР3/16 Пьянзин Илья Андреевич/task1.c
@@ -1,7 +1,38 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Найти количество элементов, расположенных между первым максимальным
 // и последним минимальным элементами.
-int task1(unsigned A[], size_t N)
-{
-  return -1;
+#include <stdio.h>
+#include <limits.h>
+#include <locale.h>
+
+int task1(unsigned A[], size_t N) {
+    if (N == 0) {
+        return -1;
+    }
+
+    int first_max_index = -1; 
+    int last_min_index = -1; 
+    unsigned max_value = 0;   
+    unsigned min_value = UINT_MAX;
+
+    for (size_t i = 0; i < N; i++) {
+        if (A[i] > max_value) {
+            max_value = A[i];
+            first_max_index = i;
+        }
+        if (A[i] <= min_value) {
+            min_value = A[i];
+            last_min_index = i;
+        }
+    }
+
+    if (first_max_index == -1 || last_min_index == -1) {
+        return -1;
+    }
+
+    if (first_max_index > last_min_index) {
+        return 0;
+    }
+
+    return last_min_index - first_max_index - 1;
 }

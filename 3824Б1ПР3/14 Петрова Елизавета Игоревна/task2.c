be4d77ae9b816a78dbed2865bf41d424392f4b47
LizaPetrova75 <lizapetrovaa132@gmail.com>
2024-12-19 10:24:31 +0300

    Update task2.c

--- 3824Б1ПР3/14 Петрова Елизавета Игоревна/task2.c
+++ 3824Б1ПР3/14 Петрова Елизавета Игоревна/task2.c
@@ -2,7 +2,38 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 1).
 // Найти длину наименьшей серии.
+#include <stddef.h>
+#include <limits.h> 
+
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+  if (n == 0) 
+  {
+    return -1; 
+  }
+  if (n == 1)
+  {
+    return 1; 
+  } 
+  int minSeries = INT_MAX; 
+  size_t currentSeries = 1; 
+  for (size_t i = 1; i < n; ++i) 
+  {
+    if (A[i] == A[i - 1]) 
+    {
+      currentSeries]++; 
+    } else 
+    {
+      if (currentSeries < minSeries) 
+      {
+        minSeries = currentSeries;
+      }
+      currentSeries = 1; 
+    }
+  }
+    if (currentSeries < minSeries) 
+    {
+      minSeries = currentSeries;
+    }
+  return minSeries;
 }

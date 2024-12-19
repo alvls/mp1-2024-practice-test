96233d4bf0ab4a54686bec0b79cf9fc4e7da840a
ProkofievEgor <fenckmmk@gmail.com>
2024-12-19 10:30:49 +0300

    Update task3.c

--- 3824Б1ФИ2/19 Прокофьев Егор Олегович/task3.c
+++ 3824Б1ФИ2/19 Прокофьев Егор Олегович/task3.c
@@ -2,18 +2,27 @@
 // (одним или несколькими). Скопировать строку s в новую и преобразовать ее так,
 // чтобы все слова в ней начинались с заглавной буквы.
 char* task3(char *s) {
-    char *result = (char*) malloc(strlen(s) + 1);
-    strcpy(result, s);
-
-    int inWord = 0;
-    for (size_t i = 0; result[i]; i++) {
-        if (isspace(result[i])) {
-            inWord = 0;
-        } else if (!inWord) {
-            inWord = 1;
-            result[i] = toupper(result[i]);
-        }
+  int len = strlen(s);
+  char *result = (char*) malloc(len + 1);
+  int newWord = 1;
+  for (size_t i = 0; i < len; i++) {
+    if (s[i] == ' ') {
+      newWord = 1;
+      result[i] = s[i];
     }
+    else if (s[i] != ' ' && newWord) {
+      newWord = 0;
+      if (s[i] >= 'a' && s[i] <= 'z') {
+        result[i] = s[i] + 'A' - 'a';
+      }
+      else {
+        result[i] = s[i];
+      }
+    }
+    else {
+      result[i] = s[i];
+    }
+  }
 
-    return result;
+  return result;
 }

c411500023f84db654e056091e21abd33924753e
NeFade <108527055+NeFade@users.noreply.github.com>
2024-12-19 10:33:16 +0300

    Update task3.c

--- 3824Б1ПР6/01 Абрахин Артем Дмитриевич/task3.c
+++ 3824Б1ПР6/01 Абрахин Артем Дмитриевич/task3.c
@@ -1,15 +1,17 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
+#include <string.h>
+#include <stdlib.h>
+
 char* task3(char* s) {
-    int len = 0;
-    while (s[len] != '\0') {
-        len++;
+    size_t len = strlen(s);
+    char* reversed = (char*)malloc((len + 1) * sizeof(char));
+
+    if (reversed == NULL) {
+        return NULL;
     }
 
-    static char reversed[100];
-    int i;
-
-    for (i = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i) {
         reversed[i] = s[len - i - 1];
     }
 

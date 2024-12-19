8216d50838155640717a9a3c43e0c5c27067f428
2blackhole <barabanegor2000@gmail.com>
2024-12-19 09:47:34 +0300

    Барабанщиков Егор

--- 3824Б1ФИ1/02 Барабанщиков Егор Евгеньевич/task1.c
+++ 3824Б1ФИ1/02 Барабанщиков Егор Евгеньевич/task1.c
@@ -1,13 +1,18 @@
 // Задача 1. Дан непустой массив A, содержащий n натуральных чисел.
 // Вернуть 1, если его элементы образуют арифметическую прогрессию,
 // иначе вернуть 0.
-int task1(unsigned A[], size_t n)
-{
-  int flag = 1;
-  for (int i = 1; i < n - 1; i++) {
-    if (A[i] - A[i - 1] != A[i + 1] -  A[i]) {
-      flag = 0;
+int task1(unsigned A[], size_t n) {
+  if (n <= 1) {
+    return 1;
+  }
+
+  int common_diff = A[1] - A[0];
+
+  for (size_t i = 1; i < n - 1; i++) {
+    if (A[i + 1] - A[i] != common_diff) {
+      return 0;
     }
   }
-  return flag;
+
+  return 1;
 }

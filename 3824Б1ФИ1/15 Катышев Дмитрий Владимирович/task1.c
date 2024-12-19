a0e606c7e7e1ad48f697e2b999c479a9391308fb
Dmitry Katyshev <181271866+Amour21@users.noreply.github.com>
2024-12-19 10:20:49 +0300

    Update task1.c

--- 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task1.c
+++ 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task1.c
@@ -3,32 +3,33 @@
 // и последним максимальным элементами.
 int task1(unsigned A[], size_t N)
 {
-  if (N < 3) 
-    return 0;
-int first_min_index = -1, last_max_index = -1;
-unsigned min = A[0], max = A[0];
-for (size_t i = 0; i < N; ++i)
-{
-    if (A[i] < min)
+    if (N < 3)
+        return 0;
+    int first_min_index = 0, last_max_index = 0;
+    unsigned min = A[0], max = A[0];
+    for (size_t i = 0; i < N; ++i)
     {
-        min = A[i];
-        first_min_index = i;
+        if (A[i] < min)
+        {
+            min = A[i];
+            first_min_index = i;
+        }
     }
-}
-for (size_t i = N - 1; i < N; --i)
-{
-    if (A[i] > max)
+    for (int i = 0; i < N; i++)  
     {
-        max = A[i];
-        last_max_index = i;
+        if (A[i] >= max)
+        {
+            max = A[i];
+            last_max_index = i;
+        }
     }
-}
-if (first_min_index < last_max_index)
-{
-    return last_max_index - first_min_index - 1;
-}
-else
-{
-    return 0; 
-}
+    if (min == max) return 0;
+    if (first_min_index < last_max_index)
+    {
+        return last_max_index - first_min_index - 1;
+    }
+    else
+    {
+        return 0;
+    }
 }

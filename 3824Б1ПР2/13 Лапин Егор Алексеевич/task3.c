d51d61325f39ed3a83312a3dc7278faf09af4315
FivelC <121977776+FivelC@users.noreply.github.com>
2024-12-19 10:34:56 +0300

    Update task3.c

--- 3824Б1ПР2/13 Лапин Егор Алексеевич/task3.c
+++ 3824Б1ПР2/13 Лапин Егор Алексеевич/task3.c
@@ -1,8 +1,17 @@
+#include <stdio.h>
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, в которой после каждого вхождения символа ch 
 // в строку s вставлен символ ‘#’.
 // Пример: ch = ‘k’, s = “abckdgkefgk”, ответ: “abck#dgk#efgk#”.
 char* task3(char *s, char ch)
 {
-  return "task3";
+  char value_swap = '#';
+  char* start = s;  
+  while (*s != '\0') {
+      if (*s == ch) {
+          *s = value_swap;  
+      }
+      s++;
+  }
+  return start; 
 }

87ac3d06eacc9e7e2b3fb653de5223193f1ad31b
Chertopoloh02 <veronika.usataya@gmail.com>
2024-12-19 09:39:51 +0300

    Update task3.c

--- 3824Б1ПР5/23 Усатая Вероника Сергеевна/task3.c
+++ 3824Б1ПР5/23 Усатая Вероника Сергеевна/task3.c
@@ -1,6 +1,17 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
-size_t task3(char *s)
+#include <stdio.h>
+#include <ctype.h>
+
+size_t task3(char* s)
 {
-  return -1;
+    size_t count = 0;
+
+    for (size_t i = 0; s[i] != '\0'; i++) {
+        if (isupper(s[i])) {
+            count++;
+        }
+    }
+
+    return count;
 }

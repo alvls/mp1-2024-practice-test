3a86f044e83bacaec557e28bf03d44134c8f8660
lestostor <olesyabogdaniha2015@gmail.com>
2024-12-19 09:32:02 +0300

    Update task1.c

--- 3824Б1ПР2/29 Щербакова Олеся Алексеевна/task1.c
+++ 3824Б1ПР2/29 Щербакова Олеся Алексеевна/task1.c
@@ -1,6 +1,15 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
+#include <limits.h>
+
 int task1(int A[], size_t n)
 {
-  return -1;
+  int a_min = INT_MAX;
+  for (int i = 0; i < n; i++) {
+  	if (A[i] < a_min) a_min = A[i];
+  }
+  
+  for (int i = n - 1; i >= 0; i--) {
+  	if (A[i] == a_min) return i + 1;
+  }
 }

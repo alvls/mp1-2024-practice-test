33898a97cb748af80eb67587036085ce6f5a6e89
IFoReZ <iliaminin123@gmail.com>
2024-12-19 10:38:55 +0300

    Update task3.c

--- 3824Б1ФИ1/20 Минин Илья Алексеевич/task3.c
+++ 3824Б1ФИ1/20 Минин Илья Алексеевич/task3.c
@@ -2,7 +2,31 @@
 // (одним или несколькими). Скопировать строку s в новую и зашифровать ее,
 // заменяя каждую букву на следующую за ней в алфавите 
 // (считать, что за ‘z’ следует ‘a’), сохраняя регистр.
-char* task3(char *s)
+
+char* task3(char* s)
 {
-  return "task3";
+  int len = 0;
+  while (s[len] != '\0')
+  {
+    len++;
+  }
+
+  char* sh = (char)malloc((len) * sizeof(char));
+
+  for (int i = 0; i < len; i++)
+  {
+    if (s[i] == 'a')
+    {
+      sh[i] = 'z';
+    }
+    else if (s[i] == 'A')
+    {
+      sh[i] = 'Z';
+    }
+    else
+    {
+      sh[i] = s[i] + 1;
+    }
+  }
+  return sh;
 }

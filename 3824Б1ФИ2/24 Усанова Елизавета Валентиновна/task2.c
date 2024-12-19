209254ddb6a20dbc8647583eb413388cf2ee6971
oduvanchik-108 <1208liza@gmail.com>
2024-12-19 10:17:08 +0300

    Update task2.c

--- 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task2.c
+++ 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task2.c
@@ -1,7 +1,47 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти количество его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
-size_t task2(double A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+#include <stdbool.h>
+
+size_t task2(double A[], size_t n) {
+    if (n <= 1) {
+        return 0; 
+    }
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0 && n > 1) 
+        {
+            if (A[i] <= A[i + 1])
+                count++;
+        }
+        else if (i == n - 1 && n > 1) 
+        {
+            if (A[i] <= A[i - 1])
+                count++;
+        }
+        else if (n > 2) 
+        {
+            if (A[i] <= A[i - 1] && A[i] <= A[i + 1])
+            {
+                count++;
+            }
+        }
+        else if (n == 2 && i == 0)
+        {
+            if (A[i] <= A[i + 1])
+            {
+                count++;
+            }
+        }
+        else if (n == 2 && i == 1)
+        {
+            if (A[i] <= A[i - 1])
+                count++;
+        }
+
+    }
+    return count;
 }

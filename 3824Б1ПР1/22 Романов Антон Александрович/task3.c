a5abee566668bd165d3c8e4b937f83b5b3155256
jst-Frenzy <lamiging@gmail.com>
2024-12-19 09:10:00 +0300

    Update task3.c

--- 3824Б1ПР1/22 Романов Антон Александрович/task3.c
+++ 3824Б1ПР1/22 Романов Антон Александрович/task3.c
@@ -1,5 +1,15 @@
 // Задача 3. Дана строка s. Подсчитать количество содержащихся в ней цифр.
+#include <ctype.h>
+#include <string.h>
+
 size_t task3(char *s)
 {
-  return -1;
+    int cnt_c = 0;
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        if (isdigit((unsigned char)s[i])) {
+            cnt_c++;
+        }
+    }
+    return cnt_c;
 }

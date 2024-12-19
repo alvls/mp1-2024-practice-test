88b1295eccab9909a85f9176e0ae283aac419c0c
lestostor <olesyabogdaniha2015@gmail.com>
2024-12-19 09:49:43 +0300

    Update task2.c

--- 3824Б1ПР2/29 Щербакова Олеся Алексеевна/task2.c
+++ 3824Б1ПР2/29 Щербакова Олеся Алексеевна/task2.c
@@ -2,7 +2,18 @@
 // Найти значение минимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
+#include <limits.h>
+
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+  unsigned long long loc_max = LLONG_MIN;
+  unsigned long long min = LLONG_MAX;
+  for (int i = 1; i < n - 1; i++) {
+    if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+      loc_max = A[i];
+      if (loc_max < min) min = loc_max;
+    }
+  }
+  if (min == LLONG_MAX) return -1;
+  return min;
 }

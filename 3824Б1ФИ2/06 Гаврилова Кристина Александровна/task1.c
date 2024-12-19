09f170f4e2974f46bbf9e5955c69cfadca09facb
Kristina-Ga <kristina_gavrilova_2006@list.ru>
2024-12-19 09:25:49 +0300

    Update task1.c

--- 3824Б1ФИ2/06 Гаврилова Кристина Александровна/task1.c
+++ 3824Б1ФИ2/06 Гаврилова Кристина Александровна/task1.c
@@ -2,5 +2,14 @@
 // Найти сколько чисел в массиве меньше своего правого соседа.
 size_t task1(int A[], size_t n)
 {
-  return -1;
+  if (n == 0 || n == 1) {
+    return 0;
+}
+int count = 0;
+for (int i = 0; i < n - 1; ++i) {
+    if (A[i] < A[i + 1]) {
+        count++;
+    }
+}
+return count;
 }

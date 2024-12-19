8f508da12d3c242760cfb9153bde5bff54832183
Анна Дырдик <anadyrdik@gmail.com>
2024-12-19 10:02:17 +0300

    Update task2.c

--- 3824Б1ПР4/09 Дырдик Анна Максимовна/task2.c
+++ 3824Б1ПР4/09 Дырдик Анна Максимовна/task2.c
@@ -1,8 +1,21 @@
-// Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
-// Найти значение минимального из его локальных максимумов.
-// Локальный максимум – элемент, который >= левого и правого соседей.
-// При отсутствии локальных максимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <limits.h>
+
+long long task2(unsigned A[], size_t n) {
+    if (n < 3) return -1;
+
+    unsigned minLocalMax = UINT_MAX;
+    int found = 0; 
+    for (size_t i = 1; i < n - 1; ++i) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+          
+            found = 1;
+            if (A[i] < minLocalMax) {
+                minLocalMax = A[i];
+            }
+        }
+    }
+
+    if (!found) return -1;
+
+    return minLocalMax;
 }

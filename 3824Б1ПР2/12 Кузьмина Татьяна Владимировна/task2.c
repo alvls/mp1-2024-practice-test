1dce86bca33f3a8980bde5efd024bf66eafd173d
Татьяна <dumpling.lol13@gmail.com>
2024-12-19 09:40:22 +0300

    Update task2.c

--- 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task2.c
+++ 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task2.c
@@ -1,15 +1,22 @@
 // Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
 // Заменить в нем все элементы, встречающиеся ровно два раза, на -1.
-void task2(int A[], size_t n)
-{
-int* count = (int*)calloc(1000000, sizeof(int)); 
-for (size_t i = 0; i < n; i++) {
-    count[A[i]]++;
-}
-for (size_t i = 0; i < n; i++) {
-    if (count[A[i]] == 2) {
-        A[i] = -1;
+#include <stdbool.h>
+
+void task2(int A[], size_t n) {
+    
+    int max_value = 1000;
+    int* count = (int*)calloc(max_value + 1, sizeof(int)); 
+
+    for (size_t i = 0; i < n; i++) {
+        count[A[i]]++;
     }
-}
-free(count);
+
+   
+    for (size_t i = 0; i < n; i++) {
+        if (count[A[i]] == 2) {
+            A[i] = -1;
+        }
+    }
+
+    free(count);
 }

1a6ab74c7165daf8360bfa6304d6b4729a87d6d3
Oxytochik <oxyrvin@gmail.com>
2024-12-19 10:37:21 +0300

    Update task3.c

--- 3824Б1ПР2/08 Иванов Илья Всеволодович/task3.c
+++ 3824Б1ПР2/08 Иванов Илья Всеволодович/task3.c
@@ -1,7 +1,20 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // строчных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "aceg".
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 char* task3(char *s)
 {
-  return "task3";
+    int ans = 0;
+    for (int i = 0; i<strlen(s); i++) {
+        if (s[i]>='a' && s[i]<='z') ans++;
+    }
+    char* h = malloc(ans+1);
+    int yk = 0;
+    for (int i = 0; i<strlen(s); i++) {
+        if (s[i]>='a' && s[i]<='z'){h[yk] = s[i];yk++;}
+    }
+    h[yk] = '\0';
+    return h;
 }

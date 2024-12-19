e3523af009435f12279fd2da093b10ec72b7e435
79502 <vladsmirnov8583@gmail.com>
2024-12-19 10:29:46 +0300

    Update task2.c
    Смирнов Владислав Владимирович

--- 3824Б1ФИ2/22 Смирнов Владислав Владимирович/task2.c
+++ 3824Б1ФИ2/22 Смирнов Владислав Владимирович/task2.c
@@ -3,6 +3,7 @@
 // расстояние |A[k1] – A[k2]| между которыми минимально.
 // Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
 // При невозможности найти пару, вернуть {-1,-1}.
+#include<math.h>
 struct Indices
 {
   int first_index;
@@ -10,6 +11,21 @@ struct Indices
 };
 struct Indices task2(double A[], size_t n)
 {
+  int diff = n;
   struct Indices indices = {-2, -2};
+  for (size_t i = 0; i < n; i++) {
+        if (A[i] == indices.first_index)
+            for (size_t j = i + 1; j < n; j++) {
+                if ((A[j] == indices.second_index) && (j - i) < diff)
+              diff = j - i;
+            }
+    }
+    if (diff == n)
+        struct Indices indices = { -1, -1 };
   return indices;
 }
+
+
+
+  
+   

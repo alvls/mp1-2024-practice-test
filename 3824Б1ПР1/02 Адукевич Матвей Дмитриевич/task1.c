0dce1cd8723401dcc003aaec42f685ed01f9223c
Matvey-Adukevich <matvey.adukevich@gmail.com>
2024-12-19 09:36:02 +0300

    Update task1.c

--- 3824Б1ПР1/02 Адукевич Матвей Дмитриевич/task1.c
+++ 3824Б1ПР1/02 Адукевич Матвей Дмитриевич/task1.c
@@ -3,5 +3,19 @@
 // иначе вернуть 0.
 int task1(unsigned A[], size_t n)
 {
-  return -1;
+	if (n == 1) {
+		return 0;
+	}
+	int a = A[1] - A[0];
+	int flag = 1;
+	for (int i = 0; i < n-1; i++) {
+		if (A[i + 1] - A[i] == a) {
+			flag = 1;
+		}
+		else {
+			flag = 0;
+			return 0;
+		}
+	}
+	return 1;
 }

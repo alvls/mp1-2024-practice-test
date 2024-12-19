bbe7905c0a3e9e7f04cc51c1a551f3239249b894
alkooo000 <vanilagas@yandex.ru>
2024-12-19 10:13:31 +0300

    Update task1.c

--- 3824Б1ПР3/20 Сагал Алина Михайловна/task1.c
+++ 3824Б1ПР3/20 Сагал Алина Михайловна/task1.c
@@ -1,7 +1,26 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Преобразовать его, прибавив к нечетным числам первый элемент,
 // а к четным – последний. Первый и последний элементы массива не менять. 
-void task1(unsigned A[], size_t N)
-{
-  A[0] = 1111111111;
+// void task1(unsigned A[], size_t N)
+// {
+//  A[0] = 1111111111;
+// }
+
+#include <stddef.h>
+
+void task1(unsigned A[], size_t N) {
+    if (N < 2) {
+        return;
+    }
+    unsigned first = A[0];
+    unsigned last = A[N - 1];
+
+    for (size_t i = 1; i < N - 1; ++i) {
+        if (A[i] % 2 == 0) {
+            A[i] += last;
+        }
+        else {
+            A[i] += first;
+        }
+    }
 }

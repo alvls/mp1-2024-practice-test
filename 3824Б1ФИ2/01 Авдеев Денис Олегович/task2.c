def418ee599a4975c78d0702472dc3b268173f4d
qwertewer <91060905+qwertewer@users.noreply.github.com>
2024-12-19 09:46:50 +0300

    Update task2.c

--- 3824Б1ФИ2/01 Авдеев Денис Олегович/task2.c
+++ 3824Б1ФИ2/01 Авдеев Денис Олегович/task2.c
@@ -3,13 +3,29 @@
 // Найти индексы двух элементов массива, сумма которых наиболее близка к числу R.
 // Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
 // При невозможности найти пару, вернуть {-1,-1}.
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
 struct Indices
 {
-  int first_index;
-  int second_index;
+	int first_index;
+	int second_index;
 };
 struct Indices task2(int A[], size_t n, double R)
 {
-  struct Indices indices = {-2, -2};
-  return indices;
+	int ind1 = 0, ind2 = 0, summa = INT_MAX;
+	if (n < 2) {
+		struct Indices a = { -1, -1 };
+		return a;
+	}
+	for (size_t i = n; i > 1; i--) {
+		for (size_t j = i - 1; j > 0; j--) {
+			if (abs(A[i]+A[j]-R) < summa){
+				ind1 = i;
+				ind2 = j;
+			}
+		}
+	}
+	struct Indices indices = { ind1, ind2 };
+	return indices;
 }

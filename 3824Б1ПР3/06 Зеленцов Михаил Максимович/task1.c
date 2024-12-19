52c1dea370fac8c325f7fa200f9e30e824422249
Ediefff <77809176+Ediefff@users.noreply.github.com>
2024-12-19 09:41:19 +0300

    Update task1.c

--- 3824Б1ПР3/06 Зеленцов Михаил Максимович/task1.c
+++ 3824Б1ПР3/06 Зеленцов Михаил Максимович/task1.c
@@ -1,6 +1,9 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего правого соседа.
-size_t task1(int A[], size_t n)
-{
-  return -1;
+size_t task1(int A[], size_t n) {
+  for (size_t i = 0; i < n - 1; i++) {
+        if (A[i] < A[i + 1]) {
+            count++;}
+  }
+  return сount;
 }

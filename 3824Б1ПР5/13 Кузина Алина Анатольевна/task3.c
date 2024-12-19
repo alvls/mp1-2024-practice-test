faa37385a7e335dd0e86549c774c159659a09cda
KuzinaAlina <aragoto2005@gmail.com>
2024-12-19 09:17:51 +0300

    Update task3.c

--- 3824Б1ПР5/13 Кузина Алина Анатольевна/task3.c
+++ 3824Б1ПР5/13 Кузина Алина Анатольевна/task3.c
@@ -2,7 +2,29 @@
 // Сформировать новую строку, в которой после каждого вхождения символа ch 
 // в строку s вставлен символ ‘#’.
 // Пример: ch = ‘k’, s = “abckdgkefgk”, ответ: “abck#dgk#efgk#”.
-char* task3(char *s, char ch)
-{
-  return "task3";
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char *s, char ch) {
+    size_t len = strlen(s);
+    size_t new_len = len;  
+    for (size_t i = 0; i < len; i++) {
+        if (s[i] == ch) {
+            new_len++;  
+        }
+    }
+    
+    char *result = (char*)malloc(new_len + 1); 
+    if (!result) return NULL; 
+
+    size_t j = 0; 
+    for (size_t i = 0; i < len; i++) {
+        result[j++] = s[i]; 
+        if (s[i] == ch) {
+            result[j++] = '#'; 
+        }
+    }
+    result[j] = '\0'; 
+    return result; 
 }

4ccad5571720c6df94631832b6b49003e1bf3c7f
Ksenia <109479530+Ksenia555@users.noreply.github.com>
2024-12-19 10:22:35 +0300

    Update task2.c

--- 3824Б1ФИ1/28 Федорова Ксения Димитриевна/task2.c
+++ 3824Б1ФИ1/28 Федорова Ксения Димитриевна/task2.c
@@ -2,7 +2,31 @@
 // Найти значение максимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
+#include <stdlib.h>
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+	int max = 0, count;
+	int* loc_max = (int*)calloc(n,sizeof(int));
+
+	count = 0;
+	for (int i = 1; i < n; i++)
+	{
+		count++;
+		if (A[i] >= A[i - 1]) loc_max[count] = A[i];
+	}
+	for (int i = 1; i < n; i++)
+	{
+		count++;
+		if (A[i] <= A[i + 1]) loc_max[count] = A[i];
+	}
+
+	
+	for (int i = 0; i < n; i++)
+	{
+		if (loc_max[i] >= max) max = loc_max[i];
+	}
+
+	if (max != 0) return max;
+	else return -1;
+	free(loc_max);
 }

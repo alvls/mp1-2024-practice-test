2706b7fa17a2231547f5edd52db05e5a52e2012f
posypr09 <aboba228332777@gmail.com>
2024-12-19 09:49:38 +0300

    Update task3.c

--- 3824Б1ПР4/20 Посыпкина Полина Романовна/task3.c
+++ 3824Б1ПР4/20 Посыпкина Полина Романовна/task3.c
@@ -2,7 +2,28 @@
 // (одним или несколькими). Скопировать строку s в новую и зашифровать ее,
 // заменяя каждую букву на следующую за ней в алфавите 
 // (считать, что за ‘z’ следует ‘a’), сохраняя регистр.
-char* task3(char *s)
-{
-  return "task3";
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char *s) {
+    size_t len = strlen(s);
+    char *encrypted = (char *)malloc((len + 1) * sizeof(char));
+
+    if (!encrypted) return NULL; // Проверка на успешное выделение памяти
+
+    for (size_t i = 0; i < len; ++i) {
+        if (isalpha(s[i])) {
+            if (islower(s[i])) {
+                encrypted[i] = (s[i] - 'a' + 1) % 26 + 'a';
+            } else if (isupper(s[i])) {
+                encrypted[i] = (s[i] - 'A' + 1) % 26 + 'A';
+            }
+        } else {
+            encrypted[i] = s[i];
+        }
+    }
+
+    encrypted[len] = '\0';
+    return encrypted;
 }

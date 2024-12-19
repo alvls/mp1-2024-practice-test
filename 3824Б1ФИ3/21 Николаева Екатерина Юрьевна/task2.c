353be60c8b7334d146e1583aa3122b78c32c6dd6
Katya01234 <3149134@gmail.com>
2024-12-19 10:39:11 +0300

    Николаева Екатерина Юрьевна 3824Б1ФИ3

--- 3824Б1ФИ3/21 Николаева Екатерина Юрьевна/task2.c
+++ 3824Б1ФИ3/21 Николаева Екатерина Юрьевна/task2.c
@@ -3,6 +3,7 @@
 // Найти индексы двух элементов массива, сумма которых наиболее близка к числу R.
 // Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
 // При невозможности найти пару, вернуть {-1,-1}.
+#include <math.h>
 struct Indices
 {
   int first_index;
@@ -10,6 +11,19 @@ struct Indices
 };
 struct Indices task2(int A[], size_t n, double R)
 {
-  struct Indices indices = {-2, -2};
+	double pogreshn = 0.000001;
+	double minim = 123456789;
+	int coord[] = { -1, -1 };
+  
+  for (int i = 0; i < n - 1; i++) {
+	  for (int j = i + 1; j < n; j++) {
+		  if (((fabs(A[i] + A[j]- R)) - minim) < pogreshn) {
+			  minim = fabs(A[i] + A[j]- R);
+			  coord[0] = i;
+			  coord[1] = j;
+		  }
+	  }
+  }
+  struct Indices indices = {coord[0],coord[1]};
   return indices;
 }

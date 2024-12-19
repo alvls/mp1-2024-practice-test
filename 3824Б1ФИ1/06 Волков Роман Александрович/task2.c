dbc154ef0a215736fa90aeb07e546bfaffe94474
Roman <91493271+tokyoview@users.noreply.github.com>
2024-12-19 09:29:07 +0300

    Update task2.c

--- 3824Б1ФИ1/06 Волков Роман Александрович/task2.c
+++ 3824Б1ФИ1/06 Волков Роман Александрович/task2.c
@@ -2,7 +2,22 @@
 // Найти значение максимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
+#include <limits.h>
+#define MIN(A, B) (A < B) ? A : B
+
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+  int i, f = 0;
+  long long min = LLONG_MAX;
+  for (i = 1; i < n-1; i++)
+  {
+    if ((A[i] <= A[i+1]) && (A[i] <= A[i-1]))
+    {
+      min = MIN(min, A[i]);
+      f = 1;
+    }
+  }
+  if (f)
+    return min;
+  return -1;
 }

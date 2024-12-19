83f158852ac5a31d482a078161992835f2327665
nawrassharaf <shrafnawras30@gmail.com>
2024-12-19 09:40:03 +0300

    Update task2.c

--- 3824Б1ПР6/27 Шараф Наурас/task2.c
+++ 3824Б1ПР6/27 Шараф Наурас/task2.c
@@ -4,5 +4,19 @@
 // При отсутствии локальных минимумов вернуть -1.
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+    long long minLocalMin = -1;
+
+    if (n < 3) {
+      return -1;
+    }
+
+    for (unsigned int i = 1; i < n - 1; i++) {
+        if (A[i] <= A[i - 1] && A[i] <= A[i + 1]) {
+            if (minLocalMin == -1 || A[i] < minLocalMin) {
+                minLocalMin = A[i];
+            }
+        }
+    }
+
+    return minLocalMin;
 }

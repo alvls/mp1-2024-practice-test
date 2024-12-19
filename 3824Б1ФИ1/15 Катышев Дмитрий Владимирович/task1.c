e6eaaa388c209eea36e24eeb9f9585e8f94b4927
Dmitry Katyshev <181271866+Amour21@users.noreply.github.com>
2024-12-19 09:24:51 +0300

    Update task1.c

--- 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task1.c
+++ 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task1.c
@@ -3,5 +3,39 @@
 // и последним максимальным элементами.
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+  if (N < 3) 
+    return 0;
+
+int first_min_index = -1, last_max_index = -1;
+unsigned min = A[0], max = A[0];
+
+
+for (size_t i = 0; i < N; ++i)
+{
+    if (A[i] < min)
+    {
+        min = A[i];
+        first_min_index = i;
+    }
+}
+
+
+for (size_t i = N - 1; i < N; --i)
+{
+    if (A[i] > max)
+    {
+        max = A[i];
+        last_max_index = i;
+    }
+}
+
+
+if (first_min_index < last_max_index)
+{
+    return last_max_index - first_min_index - 1;
+}
+else
+{
+    return 0; 
+}
 }

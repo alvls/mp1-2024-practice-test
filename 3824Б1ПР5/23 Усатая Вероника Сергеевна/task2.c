7cae4d02333e5dfd860b7aaf2fc5adfb445df4ca
Chertopoloh02 <veronika.usataya@gmail.com>
2024-12-19 09:36:56 +0300

    Update task2.c

--- 3824Б1ПР5/23 Усатая Вероника Сергеевна/task2.c
+++ 3824Б1ПР5/23 Усатая Вероника Сергеевна/task2.c
@@ -1,6 +1,28 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
+#include <stdio.h>
+#include <stdbool.h>
+
+// Функция для определения количества различных элементов массива
 size_t task2(double A[], size_t n)
 {
-  return 0;
+    size_t uniqueCount = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        bool isUnique = true;
+
+        // Проверяем, встречался ли элемент раньше
+        for (size_t j = 0; j < i; j++) {
+            if (A[i] == A[j]) {
+                isUnique = false;
+                break;
+            }
+        }
+
+        if (isUnique) {
+            uniqueCount++;
+        }
+    }
+
+    return uniqueCount;
 }

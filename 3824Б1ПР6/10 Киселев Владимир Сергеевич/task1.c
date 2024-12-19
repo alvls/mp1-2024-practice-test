7a88947bb299909aba97711ea2a2d292abe6ac1e
L3and1r <58855651+L3and1r@users.noreply.github.com>
2024-12-19 09:24:59 +0300

    Update task1.c

--- 3824Б1ПР6/10 Киселев Владимир Сергеевич/task1.c
+++ 3824Б1ПР6/10 Киселев Владимир Сергеевич/task1.c
@@ -1,6 +1,18 @@
 // Задача 1. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти номер его последнего максимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <stddef.h>
+
+int task1(int A[], size_t n) {
+    if (n == 0) {
+        return -1;
+    }
+
+    int max_index = 0;
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] >= A[max_index]) {
+            max_index = i;
+        }
+    }
+
+    return max_index;
 }

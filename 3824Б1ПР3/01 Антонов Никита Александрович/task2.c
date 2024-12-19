da62223b2a2241198eae1dc1cbfd7bf82a7d3b7b
Elestry <nikita.antonov.unn@gmail.com>
2024-12-19 10:40:57 +0300

    Update task2.c

--- 3824Б1ПР3/01 Антонов Никита Александрович/task2.c
+++ 3824Б1ПР3/01 Антонов Никита Александрович/task2.c
@@ -3,13 +3,33 @@
 // Найти индексы двух элементов массива, сумма которых наиболее близка к числу R.
 // Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
 // При невозможности найти пару, вернуть {-1,-1}.
-struct Indices
-{
-  int first_index;
-  int second_index;
+#include <math.h>
+#include <float.h>
+struct Indices {
+    int first_index;
+    int second_index;
 };
-struct Indices task2(int A[], size_t n, double R)
-{
-  struct Indices indices = {-2, -2};
-  return indices;
+
+struct Indices task2(int A[], size_t n, double R) {
+    struct Indices indices = { -1, -1 };
+    if (n < 2) {
+        return indices;
+    }
+
+    double closest_diff = DBL_MAX;
+
+    for (size_t i = 0; i < n - 1; ++i) {
+        for (size_t j = i + 1; j < n; ++j) {
+            double sum = A[i] + A[j];
+            double diff = fabs(sum - R);
+
+            if (diff < closest_diff || (diff == closest_diff && indices.first_index == -1)) {
+                closest_diff = diff;
+                indices.first_index = i;
+                indices.second_index = j;
+            }
+        }
+    }
+
+    return indices;
 }

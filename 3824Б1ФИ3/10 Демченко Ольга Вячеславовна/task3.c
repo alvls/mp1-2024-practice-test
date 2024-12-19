a688d3546dafe61f9286dd7be084bcd509a04997
OlyaDem <olga.ru.nn@icloud.com>
2024-12-19 10:29:31 +0300

    Update task3.c

--- 3824Б1ФИ3/10 Демченко Ольга Вячеславовна/task3.c
+++ 3824Б1ФИ3/10 Демченко Ольга Вячеславовна/task3.c
@@ -4,7 +4,28 @@
 //   то включить в ss N первых символов строки s.
 // Если длина строки s меньше N,
 //   то включить в ss все символы строки s, добавив после них символы '&'.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 char* task3(char *s, unsigned N)
 {
-  return "task3";
+  size_t len_s = strlen(s);
+  char *ss = (char *)malloc(sizeof(char)*(N+1));
+  size_t i = 0
+  if (len_s >= N) {
+    for (i = 0; i < N; i++) {
+      ss[i] = s[i]
+    }
+    ss[N] = '\0';
+  }
+  else {
+    for (i = 0; i < len_s; i++) {
+      ss[i] = s[i];
+    }
+    for (;i<N; i++) {
+      ss[i] = '&';
+    }
+    ss[N] = '\0';
+  }
+  return ss;
 }

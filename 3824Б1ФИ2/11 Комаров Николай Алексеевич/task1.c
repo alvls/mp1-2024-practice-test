90341e4bb1f7c4ef64c0b71fa835364aa352ac39
NikolayKomarovv <komarovl1974@gmail.com>
2024-12-19 09:12:18 +0300

    Update task1.c

--- 3824Б1ФИ2/11 Комаров Николай Алексеевич/task1.c
+++ 3824Б1ФИ2/11 Комаров Николай Алексеевич/task1.c
@@ -1,7 +1,16 @@
 // Задача 1. Даны числа a, b (0 < a < b) и непустой массив A из N элементов.
 // Найти минимальный из тех элементов A, которые содержатся в интервале (a, b).
 // Вернуть 0, если требуемые элементы отсутствуют.
-int task1(unsigned A[], size_t n, unsigned a, unsigned b)
-{
-  return -1;
+#include <stdio.h>
+#include <limits.h>
+int task1(unsigned A[], size_t n, unsigned a, unsigned b) {
+    unsigned min_element = UINT_MAX;
+    for (size_t i = 0; i < n; i++) {
+        if (A[i] > a && A[i] < b) {
+            if (A[i] < min_element) {
+                min_element = A[i];
+            }
+        }
+    }
+    return (min_element == UINT_MAX) ? 0 : min_element;
 }

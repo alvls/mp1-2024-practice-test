fedfe784510e5985043401c1066009801edfd2a3
4ne4ka <opaquellet@gmail.com>
2024-12-19 10:07:07 +0300

    Update task1.c

--- 3824Б1ПР1/18 Панова Анна Михайловна/task1.c
+++ 3824Б1ПР1/18 Панова Анна Михайловна/task1.c
@@ -1,8 +1,21 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Сформировать новый массив, в котором вначале расположены элементы массива A
 // с четными индексами, а затем – с нечетными. 
-int* task1(int A[], size_t N)
-{
-  A[0] = 1111111111;
-  return A;
+#include <malloc.h>
+int* task1(int A[], size_t N) {
+    int j = 0;
+    int* n = (int*)malloc(sizeof(int) * N);
+    if (n == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < N; i += 2) {
+        n[j] = A[i];
+        j++;
+    }
+    for (int i = 1; i < N; i += 2) {
+        n[j] = A[i];
+        j++;
+    }
+    return n;
 }

864c0b97f30f2dc1d0b85dc44062dd3f988ee428
Лызлова Злата Романовна 3824Б1ПР1 <lyzlova.zlata@gmail.com>
2024-12-19 09:31:42 +0300

    Update task1.c

--- 3824Б1ПР1/14 Лызлова Злата Романовна/task1.c
+++ 3824Б1ПР1/14 Лызлова Злата Романовна/task1.c
@@ -3,5 +3,21 @@
 // элемента в нем.
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+	size_t ind = 0;
+	unsigned mn = A[0];
+	unsigned mx = A[0];
+	for (size_t i = 1; i < N; i++)
+	{
+		if (A[i] <= mn)
+		{
+			mn = A[i];
+			ind = i;
+		}
+		if (A[i] >= mx)
+		{
+			mx = A[i];
+			ind = i;
+		}
+	}
+	return ind;
 }

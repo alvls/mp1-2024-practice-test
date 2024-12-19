92dc6cab376f4a845abdddab619a66a9bd2b36ac
Micro4elikkk <95868942+Micro4elikkk@users.noreply.github.com>
2024-12-19 09:31:01 +0300

    Update task2.c

--- 3824Б1ПР1/23 Русановский Никита Владимирович/task2.c
+++ 3824Б1ПР1/23 Русановский Никита Владимирович/task2.c
@@ -1,6 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
+int search(double A[], size_t n, double elem) {
+	for (size_t i = 0; i < n; i++) if (elem == A[i]) return 1;
+	return 0;
+}
 size_t task2(double A[], size_t n)
 {
-  return 0;
+	double* temp = (double*)malloc(sizeof(double) * n);
+	int cntr = 0;
+	for (size_t i = 0; i < n; i++) {
+		if(search(temp, cntr+1, A[i])==0) {
+			cntr += 1;
+			temp[cntr] = A[i];
+		}
+	}
+	free(temp);
+	return cntr;
 }

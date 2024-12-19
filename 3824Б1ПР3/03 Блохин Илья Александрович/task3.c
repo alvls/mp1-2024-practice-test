2c69fde39dd3131fd7b20a05779a584ce6d761cd
IntFxZen <86683282+IntFxZen@users.noreply.github.com>
2024-12-19 10:05:56 +0300

    Update task3.c

--- 3824Б1ПР3/03 Блохин Илья Александрович/task3.c
+++ 3824Б1ПР3/03 Блохин Илья Александрович/task3.c
@@ -1,9 +1,7 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
-#include <stddef.h>
-size_t task3(char *s)
-{
-    size_t count = 0;
+int count_uppercase_letters(const char *s) {
+    int count = 0;
     while (*s) {
         if (*s >= 'A' && *s <= 'Z') {
             count++;

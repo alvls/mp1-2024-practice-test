a32c8e6e1db087e026606c6293fa1c9482474dee
sannarat <sannarat45@gmail.com>
2024-12-19 09:59:32 +0300

    Update task3.c

--- 3824Б1ПР2/01 Боронин Егор Алексеевич/task3.c
+++ 3824Б1ПР2/01 Боронин Егор Алексеевич/task3.c
@@ -1,6 +1,18 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
+#include <string.h> 
+#include <stdlib.h> 
 char* task3(char *s)
 {
-  return "task3";
+  if (s == NULL) return 0;
+
+  size_t len = strlen(s);
+  char* revers = (char*)malloc(sizeof(char) * (len + 1));
+
+  for (int i = 0; i < len;i++)
+  {
+    revers[i] = s[len - 1 - i];
+  }
+  revers[len] = '\0';
+  return revers;
 }

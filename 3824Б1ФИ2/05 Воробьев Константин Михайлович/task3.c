737900e20798a036926980d9dffa66b1aff49cc7
Kostyahard222 <84705935+Kostyahard222@users.noreply.github.com>
2024-12-19 10:14:40 +0300

    Update task3.c

--- 3824Б1ФИ2/05 Воробьев Константин Михайлович/task3.c
+++ 3824Б1ФИ2/05 Воробьев Константин Михайлович/task3.c
@@ -1,6 +1,18 @@
-// Задача 3. Дано целое число N. Не используя функции стандартной библиотеки
-// языка С, перевести число в строковое представление в десятичном виде.
+#include <stdio.h>
 char* task3(int N)
 {
-  return "task3";
+    int leng=0,cl=N;
+    while(cl!=0){
+        cl=cl/10;
+        leng++;
+    }
+    cl=N;
+    char h;
+    char* c = (char*)malloc(leng * sizeof(char));
+    for(int i=leng-1;i>=0;i--){
+        h=(cl%10)+'0';
+        c[i]=h;
+        cl=cl/10;
+    }
+    return c;
 }

ed8b13a1f1e48f3706694fb1e42592ed31845c41
catafalqueee <agenngolub@gmail.com>
2024-12-19 10:29:52 +0300

    Update task3.c

--- 3824Б1ФИ3/28 Чернопазов Даниил Александрович/task3.c
+++ 3824Б1ФИ3/28 Чернопазов Даниил Александрович/task3.c
@@ -1,7 +1,30 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // строчных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "aceg".
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX_RESULT_SIZE 256
+
 char* task3(char *s)
 {
-  return "task3";
+  static char result[MAX_RESULT_SIZE]; 
+  size_t j = 0; 
+
+  memset(result, 0, sizeof(result));
+
+  for (size_t i = 0; i < strlen(s); i++) {
+      if (islower(s[i])) {
+          if (j < MAX_RESULT_SIZE - 1) { 
+              result[j] = s[i];
+              j++;
+          }
+          else {
+              break;
+          }
+      }
+  }
+
+  result[j] = '\0'; 
+  return result;
 }

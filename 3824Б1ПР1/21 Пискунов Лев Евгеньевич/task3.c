9bf20e0f3ca8a031f9193ec2e8040419973a7435
LCDIIL <100310697+LCDIIL@users.noreply.github.com>
2024-12-19 10:35:32 +0300

    Update task3.c

--- 3824Б1ПР1/21 Пискунов Лев Евгеньевич/task3.c
+++ 3824Б1ПР1/21 Пискунов Лев Евгеньевич/task3.c
@@ -6,15 +6,16 @@
 
 char* task3(char* s) {
 
- int li = -1, k = 0;
- for (int i = 0; s[i] != '\0'; i++) li++;
- char* res = (char*)malloc(sizeof(char) * (li + 2));
- for (int i = li; i > -1; i--) {
-  res[k] = s[i];
-  k++;
- }
- res[li + 1] = '\0';
- for (int i = 0; res[i] != '\0'; i++) s[i] = res[i];
- free(res);
- return s;
+  int li = -1, k = 0;
+  for (int i = 0; s[i] != '\0'; i++) li++;
+  char* res = (char*)malloc(sizeof(char) * (li + 2));
+  for (int i = li; i > -1; i--)
+  {
+   res[k] = s[i];
+   k++;
+  }
+  res[li + 1] = '\0';
+  for (int i = 0; res[i] != '\0'; i++) s[i] = res[i];
+  free(res);
+  return s;
 }

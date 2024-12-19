34471b36eb5d3a1dab1a2accca6e3a732fb597c5
NititaTch <136841289+MNikitattt@users.noreply.github.com>
2024-12-19 10:22:10 +0300

    Update task3.c

--- 3824Б1ФИ2/30 Щербаков Никита Андреевич/task3.c
+++ 3824Б1ФИ2/30 Щербаков Никита Андреевич/task3.c
@@ -10,5 +10,27 @@
 //   то включить в ss все символы строки s, добавив после них символы '&'.
 char* task3(char *s, unsigned N)
 {
-  return "task3";
+	int len = strlen(s);
+	if (len >= N)
+	{
+		char* str = (char*)calloc(N+1, sizeof(char));
+		if (str == NULL)
+		{
+			exit(EXIT_FAILURE);
+		}
+		strncpy(str, s, N);
+		return str;
+	}
+	if (len < N)
+	{
+		char* str = (char*)calloc(len + 2, sizeof(char));
+		if (str == NULL)
+		{
+			exit(EXIT_FAILURE);
+		}
+		strncpy(str, s, len);
+		str[len] = '&';
+		str[len + 1] = '\0';
+		return str;
+	}
 }

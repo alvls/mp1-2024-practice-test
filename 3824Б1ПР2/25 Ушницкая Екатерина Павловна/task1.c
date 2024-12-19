ffaf3709a1988559d7120e5c61593feabac62350
Екатерина Ушницкая <ushnitskaya.ekaterina@mail.ru>
2024-12-19 09:32:18 +0300

    Update task1.c

--- 3824Б1ПР2/25 Ушницкая Екатерина Павловна/task1.c
+++ 3824Б1ПР2/25 Ушницкая Екатерина Павловна/task1.c
@@ -1,6 +1,17 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего левого соседа.
-size_t task1(int A[], size_t n)
-{
-  return -1;
+
+size_t task1(int A[], size_t n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    size_t count_of_left = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (A[i] < A[i - 1]) { 
+            count_of_left++;
+        }
+    }
+    return count_of_left; 
 }

af613b04d03620ac4568d0c4b2bb51c1e3d8ba1d
elenakarpova0 <lightnessss21@gmail.com>
2024-12-19 10:37:29 +0300

    Update task1.c

--- 3824Б1ФИ3/14 Карпова Елена Александровна/task1.c
+++ 3824Б1ФИ3/14 Карпова Елена Александровна/task1.c
@@ -1,7 +1,29 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Найти номер последнего экстремального (минимального или максимального) 
 // элемента в нем.
+
+#include <stdlib.h>
+
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+    if (N == 0) {
+        return -1; 
+    }
+
+    unsigned min_val = A[0]; 
+    unsigned max_val = A[0];
+    int last_extreme_index = 0;
+
+    for (size_t i = 1; i < N; i++) {
+        if (A[i] < min_val) {
+            min_val = A[i];
+            last_extreme_index = i;
+        }
+        if (A[i] > max_val) {
+            max_val = A[i];
+            last_extreme_index = i;
+        }
+    }
+
+    return last_extreme_index;
 }

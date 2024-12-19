2c6398c6a8de4818b67753f9a467ddb8893325b8
Mercurial00 <148081131+Mercurial00@users.noreply.github.com>
2024-12-19 09:58:09 +0300

    Update task3.c

--- 3824Б1ФИ2/23 Старостин Данил Денисович/task3.c
+++ 3824Б1ФИ2/23 Старостин Данил Денисович/task3.c
@@ -1,6 +1,15 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
-size_t task3(char *s)
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+size_t task3(char* s)
 {
-  return -1;
+	int res = 0;
+	for (size_t i = 0; i < strlen(s); ++i) {
+		if (isalpha(s[i]) && isupper(s[i])) {
+			++res;
+		}
+	}
+	return res;
 }

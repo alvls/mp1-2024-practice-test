8adcf97feaeac6856af098cc2fcfb082bb9d8005
Даниил Ципин <daniiltsipin@gmail.com>
2024-12-19 10:01:42 +0300

    Update task1.c

--- 3824Б1ФИ2/27 Ципин Даниил Денисович/task1.c
+++ 3824Б1ФИ2/27 Ципин Даниил Денисович/task1.c
@@ -1,16 +1,19 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между первыми
 // двумя нулями.
+#include <stdio.h> 
+#include <stddef.h>
+
 int task1(int A[], size_t n) {
   int sum = 0;
   char first_zero_found = 0;
   for (size_t i = 0; i < n; ++i) {
     if (A[i] == 0) {
       if (!first_zero_found) {
-        first_zero_found = true;
+        first_zero_found = 1;
       }
       else {
-        break; второй ноль найден, окончание суммирования
+        break; //второй ноль найден, окончание суммирования
       }
     } else if (first_zero_found) {
       sum += A[i];

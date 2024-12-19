bbf742c1b18221bf7b34edb905e1261b106f492a
Basic <51361929+seiftnesse@users.noreply.github.com>
2024-12-18 23:10:33 -0800

    Баландин Владислав Вячеславович

--- 3824Б1ПР5/02 Баландин Владислав Вячеславович/task1.c
+++ 3824Б1ПР5/02 Баландин Владислав Вячеславович/task1.c
@@ -1,7 +1,29 @@
 // Задача 1. Дан непустой массив A, содержащий n натуральных чисел.
 // Вернуть 1, если его элементы образуют арифметическую прогрессию,
 // иначе вернуть 0.
+#include <stddef.h>
+
 int task1(unsigned A[], size_t n)
 {
-  return -1;
+  if (n <= 1)
+  {
+    return 0;
+  }
+
+  if (n == 2)
+  {
+    return 1;
+  }
+
+  const int difference = (int)A[1] - (int)A[0];
+
+  for (size_t i = 2; i < n; i++)
+  {
+    if ((int)A[i] - (int)A[i - 1] != difference)
+    {
+      return 0;
+    }
+  }
+
+  return 1;
 }
--- 3824Б1ПР5/02 Баландин Владислав Вячеславович/task2.c
+++ 3824Б1ПР5/02 Баландин Владислав Вячеславович/task2.c
@@ -8,8 +8,45 @@ struct Indices
   int first_index;
   int second_index;
 };
+
+#include <math.h>
+
+#include <stddef.h>
+
 struct Indices task2(double A[], size_t n)
 {
-  struct Indices indices = {-2, -2};
-  return indices;
+  struct Indices result;
+
+  if (n < 2)
+  {
+    result.first_index = -1;
+    result.second_index = -1;
+    return result;
+  }
+
+  int k1 = 0;
+  int k2 = 1;
+  double min_distance = fabs(A[1] - A[0]);
+
+  for (size_t i = 0; i < n; i++)
+  {
+    for (size_t j = i + 1; j < n; j++)
+    {
+      const double current_distance = fabs(A[j] - A[i]);
+
+      const double EPSILON = 1e-10;
+
+      if (current_distance < min_distance - EPSILON ||
+          (fabs(current_distance - min_distance) < EPSILON && i < (size_t)k1))
+      {
+        min_distance = current_distance;
+        k1 = i;
+        k2 = j;
+      }
+    }
+  }
+
+  result.first_index = k1;
+  result.second_index = k2;
+  return result;
 }

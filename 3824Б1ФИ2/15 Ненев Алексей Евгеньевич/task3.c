7411803821b198c37cbb73772aeb144bde188652
para5ol <an02.02@mail.ru>
2024-12-19 10:16:21 +0300

    Update task3.c

--- 3824Б1ФИ2/15 Ненев Алексей Евгеньевич/task3.c
+++ 3824Б1ФИ2/15 Ненев Алексей Евгеньевич/task3.c
@@ -1,7 +1,18 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, из которой удалены все вхождения символа ch. 
 // Пример: ch = ‘k’, s = “kabckdgkefgk”, ответ: “abcdgefg”.
+#include <stdio.h>
+#include <string.h>
 char* task3(char *s, char ch)
 {
-  return "task3";
+    char* src = s;
+    char* dst = s;
+    while (*src) {
+        if (*src != ch) {
+            *dst++ = *src;
+        }
+        src++;
+    }
+    *dst = '\0';
+    return s;
 }

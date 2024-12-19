902ba38d8685c376152f4e8a9c60a707d763bc8d
Daemos17 <89529780+Daemos17@users.noreply.github.com>
2024-12-19 10:22:23 +0300

    Update task2.c

--- 3824Б1ПР4/10 Казанцев Даниил Алексеевич/task2.c
+++ 3824Б1ПР4/10 Казанцев Даниил Алексеевич/task2.c
@@ -1,6 +1,26 @@
+#include <stdlib.h>
 // Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
 // Заменить в нем все элементы, встречающиеся менее двух раз, на -1.
 void task2(int A[], size_t n)
 {
-  A[0] = 1111111111;
+   int max_value = 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] > max_value) {
+            max_value = A[i];
+        }
+    }
+    int* counts = (int*)calloc(max_value + 1, sizeof(int));
+    if (!counts) {
+        printf("Ошибка выделения памяти\n");
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        counts[A[i]]++;
+    }    
+    for (int i = 0; i < n; i++) {
+        if (counts[A[i]] < 2) {
+            A[i] = -1;
+        }
+    }
+    free(counts);
 }

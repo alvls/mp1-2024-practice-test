bdb0b2fa53bb97e7d65b93e246d823948c5789ac
perf1lev <perfilevivan72@gmail.com>
2024-12-19 09:47:20 +0300

    Update task2.c

--- 3824Б1ФИ3/24 Перфильев Иван Павлович/task2.c
+++ 3824Б1ФИ3/24 Перфильев Иван Павлович/task2.c
@@ -1,7 +1,23 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти количество его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
-size_t task2(double A[], size_t n)
-{
-  return -1;
+#include <stddef.h>
+size_t task2(double A[], size_t n) {
+    if (n < 3) {
+        return -1; 
+    }
+
+    size_t t = 0; 
+
+    for (size_t i = 1; i < n - 1; ++i) {
+        if (A[i] <= A[i - 1] && A[i] <= A[i + 1]) {
+            t++;
+        }
+    }
+
+    if (t == 0) {
+        return -1; 
+    }
+
+    return t; 
 }

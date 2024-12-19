c44ca3959b4f341dc4de178578942845f88763f1
Taino Dergal <fedadekterev3@gmail.com>
2024-12-19 09:32:16 +0300

    Update task2.c

--- 3824Б1ПР6/06 Дектерёв Фёдор Егорович/task2.c
+++ 3824Б1ПР6/06 Дектерёв Фёдор Егорович/task2.c
@@ -3,19 +3,17 @@
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
 long long task2(unsigned A[], size_t n) {
-    if (n < 1) {
-        return -1;
+    if (n == 0) {
+        return 0;
     }
-
     long long max_local_min = -1;
-
+    
     if (n > 1 && A[0] <= A[1]) {
         max_local_min = A[0];
     }
 
     for (size_t i = 1; i < n - 1; i++) {
         if (A[i] <= A[i - 1] && A[i] <= A[i + 1]) {
-          
             if (A[i] > max_local_min) {
                 max_local_min = A[i];
             }
@@ -28,5 +26,5 @@ long long task2(unsigned A[], size_t n) {
         }
     }
 
-    return max_local_min;
+    return (max_local_min == -1) ? 0 : max_local_min;
 }

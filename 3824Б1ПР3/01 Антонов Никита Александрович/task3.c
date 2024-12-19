f6b674558cbf3267f49436c6f71cf0da9a81e777
Elestry <nikita.antonov.unn@gmail.com>
2024-12-19 10:38:04 +0300

    Update task3.c

--- 3824Б1ПР3/01 Антонов Никита Александрович/task3.c
+++ 3824Б1ПР3/01 Антонов Никита Александрович/task3.c
@@ -1,6 +1,25 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
-char* task3(char *s)
-{
-  return "task3";
+#include <string.h>
+#include <stdlib.h>
+char* task3(char* s) {
+    if (!s) {
+        return NULL;
+    }
+
+    size_t len = strlen(s);
+    char* reversed = (char*)malloc((len + 1) * sizeof(char));
+    if (!reversed) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; ++i) {
+        reversed[i] = s[len - 1 - i];
+    }
+    reversed[len] = '\0';
+
+    return reversed;
+}
+void main(){
+
 }

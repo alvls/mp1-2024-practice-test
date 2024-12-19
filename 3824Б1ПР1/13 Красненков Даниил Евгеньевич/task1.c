f8cdf21223965231fbbdd6a17c3969e92b0fba88
DaniilKrasnenkov81 <krasnenkovdaniil1@gmail.com>
2024-12-19 10:37:24 +0300

    Update task1.c

--- 3824Б1ПР1/13 Красненков Даниил Евгеньевич/task1.c
+++ 3824Б1ПР1/13 Красненков Даниил Евгеньевич/task1.c
@@ -3,5 +3,22 @@
 // элемента в нем.
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+    unsigned min = A[0], max = A[0];
+    size_t min_idx = 0, max_idx = 0;
+    for (size_t i = 1; i < N; i++) {
+        if (A[i] < min) {
+            min = A[i];
+            min_idx = i;
+        }
+        if (A[i] > max) {
+            max = A[i];
+            max_idx = i;
+        }
+    }
+    if (min_idx < max_idx) {
+        return min_idx;
+    }
+    else {
+        return max_idx;
+    }
 }

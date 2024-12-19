6e91895b5073ea2a7dff62681752526cfc576701
aaaaaaa <64205016+stalker238@users.noreply.github.com>
2024-12-19 10:33:54 +0300

    Update task3.c
    Павельев Григорий

--- 3824Б1ПР4/19 Павельев Григорий Михайлович/task3.c
+++ 3824Б1ПР4/19 Павельев Григорий Михайлович/task3.c
@@ -1,7 +1,19 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Скопировать строку s в новую и преобразовать ее так,
 // чтобы все слова в ней начинались с заглавной буквы.
+#include <ctype.h>
 char* task3(char *s)
 {
-  return "task3";
+  int lenn = strlen(s);
+  int z = 1;
+  char out;
+  for(int i = 0; i<lenn; i++){
+    if(s[i] == ' ') out[i] = ' ';
+    if(z){
+      out[i] = toupper(s[i]);
+      z = 0;
+    }
+    else out[i] = s[i];
+  }
+  return out;
 }

15ecab93e3214a8fb0caaeb35df0c332f310ad40
Yournoy <taras7777feduloff@gmail.com>
2024-12-19 09:59:33 +0300

    Update task1.c

--- 3824Б1ПР4/23 Федулов Тарас Денисович/task1.c
+++ 3824Б1ПР4/23 Федулов Тарас Денисович/task1.c
@@ -1,6 +1,15 @@
 // Задача 1. Дан непустой массив A, содержащий n натуральных чисел.
 // Вычислить сумму S тех элементов массива, значения которых совпадают с их индексами.
+#include <stdio.h>
 unsigned task1(unsigned A[], size_t n)
 {
-  return -1;
+  unsigned sum = 0;
+  for (size_t i = 0; i < n; i++) 
+  {
+       if (A[i] == i)
+       {
+         sum = sum + A[i];
+       }
+  }
+  return sum;
 }

db1777349cd205627fcb5792b3703119a9fe6e93
IntFxZen <86683282+IntFxZen@users.noreply.github.com>
2024-12-19 09:37:07 +0300

    Update task2.c

--- 3824Б1ПР3/03 Блохин Илья Александрович/task2.c
+++ 3824Б1ПР3/03 Блохин Илья Александрович/task2.c
@@ -3,8 +3,7 @@
 #include <stddef.h>
 size_t task2(double A[], size_t n)
 {
-      size_t unique_count = 0;
-
+    size_t unique_count = 0;
     for (size_t i = 0; i < n; ++i) {
         int is_unique = 1;
         for (size_t j = 0; j < unique_count; ++j) {
@@ -14,8 +13,8 @@ size_t task2(double A[], size_t n)
             }
         }
         if (is_unique) {
-            A[unique_count] = A[i]; 
-            unique_count++; 
+            A[unique_count] = A[i];
+            unique_count++;
         }
     }
     return unique_count;

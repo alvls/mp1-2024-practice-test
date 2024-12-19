28ca5759c569043850c2fe1266c717317ab9c9f4
Sashaandreev1 <sashaand0505@gmail.com>
2024-12-19 09:56:09 +0300

    Update task3.c

--- 3824Б1ПР4/01 Андреев Александр Олегович/task3.c
+++ 3824Б1ПР4/01 Андреев Александр Олегович/task3.c
@@ -1,6 +1,27 @@
-// Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
-// но расположенные в обратном порядке.
+#include <stdlib.h>
+#include <string.h>
+
 char* task3(char *s)
 {
-  return "task3";
+    if (s == NULL) // Проверка на NULL
+    {
+        return NULL;
+    }
+
+    size_t len = strlen(s); // Получаем длину исходной строки
+    char *reversed = (char*)malloc(len + 1); // Выделяем память для новой строки (+1 для '0')
+
+    if (reversed == NULL) // Проверка на успешное выделение памяти
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; i++)
+    {
+        reversed[i] = s[len - 1 - i]; // Копируем символы в обратном порядке
+    }
+
+    reversed[len] = '0'; // Завершаем новую строку нулевым символом
+
+    return reversed; // Возвращаем указатель на новую строку
 }

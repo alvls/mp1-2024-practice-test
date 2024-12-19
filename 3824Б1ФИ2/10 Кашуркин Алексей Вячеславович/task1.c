836f223d59ac86c5d02596c31d1bf8ac824406e6
mld8n13k <akashurkin2016@ya.ru>
2024-12-19 09:23:06 +0300

    Update task1.c

--- 3824Б1ФИ2/10 Кашуркин Алексей Вячеславович/task1.c
+++ 3824Б1ФИ2/10 Кашуркин Алексей Вячеславович/task1.c
@@ -1,6 +1,13 @@
 // Задача 1. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти номер его последнего максимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+int task1(int A[], size_t n) {
+    if (n == 0) return -1;
+    int maxIndex = 0;
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] >= A[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
 }

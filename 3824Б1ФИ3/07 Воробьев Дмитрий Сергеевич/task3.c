3d93dadbb8318b484f95cdc0db38abacb4aca7ad
Vorobyov05 <alekseevaekaterina506@gmail.com>
2024-12-19 10:15:09 +0300

    Update task3.c

--- 3824Б1ФИ3/07 Воробьев Дмитрий Сергеевич/task3.c
+++ 3824Б1ФИ3/07 Воробьев Дмитрий Сергеевич/task3.c
@@ -1,28 +1,17 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // заглавных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "BDF".
-char* task3(char *s)
+№include <string.h>
+char* task3(char *s, char *res)
 {
-	int len = 0, j = 0;
+	int j = 0;
 	for (int i = 0; s[i] != '\0'; i++)
-	{
-		if (isupper(s[i]))
 		{
-			len++
+			if (s[i] >= 'A' && s[i] <= 'Z')
+			{
+				res[j] = s[i];
+				j++;
+			}
 		}
-	}
-	char* res = (char*)malloc((len + 1) * sizeof(char));
-	if (res == NULL)
-	{
-		return NULL;
-	}
-	for (int i = 0; s[i] != '\0'; i++)
-	{
-		if (isupper(s[i]))
-		{
-			res[j++] = s[i];
-		}
-	}
 	res[j] = '\0';
-	return res;
 }

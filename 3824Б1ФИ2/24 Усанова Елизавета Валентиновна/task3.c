682f0f5731304e66ccfe5010fa730379a6d8b392
oduvanchik-108 <1208liza@gmail.com>
2024-12-19 10:31:05 +0300

    Update task3.c

--- 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task3.c
+++ 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task3.c
@@ -1,6 +1,16 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней строчных букв английского алфавита.
-size_t task3(char *s)
-{
-  return -1;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+size_t task3(char* s) {
+    int count = 0;
+    if (s == NULL) return 0; 
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (islower(s[i])) {
+            count++;
+        }
+    }
+    return count;
 }

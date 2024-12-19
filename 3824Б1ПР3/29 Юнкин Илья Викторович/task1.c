b8b13b98a9c9c856c687378fb8e2d895cfe8b34b
SyntaxErrorGo <ila.26russ@gmail.com>
2024-12-19 10:11:15 +0300

    Update task1.c

--- 3824Б1ПР3/29 Юнкин Илья Викторович/task1.c
+++ 3824Б1ПР3/29 Юнкин Илья Викторович/task1.c
@@ -2,5 +2,19 @@
 // Найти номер его последнего минимального элемента.
 int task1(int A[], size_t n)
 {
-  return -1;
+    int min_value = A[0];
+    int min_index = 0;
+    for(size_t i = 0; i < n; i++)
+    {
+        if(A[i] < min_value)
+        {
+            min_value = A[i];
+            min_index = i;
+        }
+        else if(A[i] == min_value)
+        {
+            min_index = i; // Обновляем индекс на текущий, так как ищем последний минимальный элемент
+        }
+    }
+    return min_index;
 }

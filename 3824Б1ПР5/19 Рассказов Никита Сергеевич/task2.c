2f574b0af74ca9c54ec5e74440ece30665a4fe69
mem0ry11 <nikita.rasskazov20061@gmail.com>
2024-12-19 10:20:40 +0300

    Рассказов Никита Сергеевич 3824Б1ПР5 19 вар 2 задание.c

--- 3824Б1ПР5/19 Рассказов Никита Сергеевич/task2.c
+++ 3824Б1ПР5/19 Рассказов Никита Сергеевич/task2.c
@@ -2,7 +2,33 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 2).
 // Найти число серий длины 2 в массиве.
-int task2(unsigned A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+#include <stdlib.h>
+
+int task2(unsigned A[], size_t n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    int count = 0;
+    unsigned current = A[0];
+    size_t run_length = 1;
+
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] == current) {
+            run_length++;
+        }
+        else {
+            if (run_length == 2) {
+                count++;
+            }
+            run_length = 1;
+        }
+    }
+
+    if (run_length == 2) {
+        count++;
+    }
+
+    return count;
 }

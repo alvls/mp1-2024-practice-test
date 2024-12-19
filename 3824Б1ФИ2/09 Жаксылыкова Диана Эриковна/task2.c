b976cf82ec9d79a41ccdcef1c4b28a79c29bd971
Диана <113378257+hwelq@users.noreply.github.com>
2024-12-19 09:35:33 +0300

    Update task2.c

--- 3824Б1ФИ2/09 Жаксылыкова Диана Эриковна/task2.c
+++ 3824Б1ФИ2/09 Жаксылыкова Диана Эриковна/task2.c
@@ -2,7 +2,17 @@
 // Найти значение минимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+long long task2(unsigned A[], size_t n) {
+    if (n < 3) {
+        return -1;
+    }
+    long long min_local_max = -1;
+    for (size_t i = 1; i < n - 1; i++) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+            if (min_local_max == -1 || A[i] < min_local_max) {
+                min_local_max = A[i]; 
+            }
+        }
+    }
+    return min_local_max; 
 }

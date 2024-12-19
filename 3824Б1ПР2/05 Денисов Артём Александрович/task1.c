f62f16d8a728b2c97682a60b49244a3fe7e02605
_Mindal <79373184+Mindally@users.noreply.github.com>
2024-12-19 10:10:57 +0300

    Update task1.c

--- 3824Б1ПР2/05 Денисов Артём Александрович/task1.c
+++ 3824Б1ПР2/05 Денисов Артём Александрович/task1.c
@@ -1,6 +1,11 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего левого соседа.
+#include <stdlib.h>
 size_t task1(int A[], size_t n)
 {
-  return -1;
+    size_t cnt = 0;
+    for (int i = 1; i < n; i++) {
+        if (A[i] < A[i - 1]) cnt++;
+    }
+    return cnt;
 }

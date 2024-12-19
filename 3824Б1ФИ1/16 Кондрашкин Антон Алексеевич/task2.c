9a6abb5243e5cfda94b68e7e0da4ab34f75860c5
Антон Кондрашкин <821019@bk.ru>
2024-12-19 10:32:56 +0300

    Update task2.c

--- 3824Б1ФИ1/16 Кондрашкин Антон Алексеевич/task2.c
+++ 3824Б1ФИ1/16 Кондрашкин Антон Алексеевич/task2.c
@@ -2,7 +2,21 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 2).
 // Найти число серий в массиве.
-int task2(unsigned A[], size_t n)
-{
-  return -1;
+int task2(unsigned A[], size_t n) {
+    if (n < 2) {
+        return 0;
+    }
+    int series_count = 0, i = 0;
+    while (i < n) {
+        int series_length = 1;
+        while (i + 1 < n && A[i] == A[i + 1]) {
+            series_length++;
+            i++;
+        }
+        if (series_length >= 2) {
+            series_count++;
+        }
+        i++;
+    }
+    return series_count;
 }

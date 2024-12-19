971bedeac77ebc9c9279906aed7d6a159ac94a0f
SkyTrave1er <ivan31esm@gmail.com>
2024-12-19 10:26:36 +0300

    Update task3.c

--- 3824Б1ПР2/06 Есменов Иван Сергеевич/task3.c
+++ 3824Б1ПР2/06 Есменов Иван Сергеевич/task3.c
@@ -3,5 +3,17 @@
 // Пример: s = "abc1d2e34f56g", ответ: "123456".
 char* task3(char *s)
 {
-  return "task3";
+  size_t len = strlen(s);
+  char* res = (char*)malloc(len + 1);
+  if (!res) {
+    return NULL;
+  }
+  size_t x = 0;
+  for (size_t i = 0; i < len; ++i) {
+    if (s[i] >= '0' && s[i] <= '9') {
+      res[x++] = s[i];
+    }
+  }
+  res[x] = '\0';
+  return res;  
 }

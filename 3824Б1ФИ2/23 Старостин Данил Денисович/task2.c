197cecd8c8a13100edfc922f9a2a5888426ff06e
Mercurial00 <148081131+Mercurial00@users.noreply.github.com>
2024-12-19 09:43:06 +0300

    Update task2.c

--- 3824Б1ФИ2/23 Старостин Данил Денисович/task2.c
+++ 3824Б1ФИ2/23 Старостин Данил Денисович/task2.c
@@ -1,6 +1,20 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
+#include <stdio.h>
 size_t task2(double A[], size_t n)
 {
-  return 0;
+	int found = 0;
+	size_t res = 0;
+	for (size_t i = 0; i < n; ++i) {
+		for (size_t j = 0; j < n; ++j) {
+			if (j != i && A[j] == A[i]) {
+				found = 1;
+			}
+		}
+		if (!found) {
+			++res;	
+		}
+		found = 0;
+	}
+	return res;
 }

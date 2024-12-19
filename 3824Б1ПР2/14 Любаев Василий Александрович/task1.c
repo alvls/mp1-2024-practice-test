2ec7a2760ede44a43c0ba90ed0b9f1cf08a5e54d
Avayll <91011499+Avayll@users.noreply.github.com>
2024-12-19 09:56:41 +0300

    Update task1.c

--- 3824Б1ПР2/14 Любаев Василий Александрович/task1.c
+++ 3824Б1ПР2/14 Любаев Василий Александрович/task1.c
@@ -1,7 +1,15 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Найти номер последнего экстремального (минимального или максимального) 
 // элемента в нем.
-int task1(unsigned A[], size_t N)
+#include <stdio.h>
+
+int task1(int* A, size_t N)
 {
-  return -1;
+  int big = -100000, smol = 100000; //костыль чтобы big и smol попадали в большинство элементов
+  for (int i = 0; i < N; i++) {
+    if (A[i] < smol) { smol = A[i]; }
+    else if (A[i] > big) { big = A[i]; }
+  }
+  printf("\nbig = %d; smol = %d", big, smol);
+  return big, smol;
 }

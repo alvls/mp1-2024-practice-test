bd6d9d47d45e18e1ac30b80ca7f3e05f221d4cf0
dariatsyganovaa <dcyganova857@gmail.com>
2024-12-19 09:56:30 +0300

    Update task2.c

--- 3824Б1ПР2/26 Цыганова Дарья Александровна/task2.c
+++ 3824Б1ПР2/26 Цыганова Дарья Александровна/task2.c
@@ -2,7 +2,19 @@
 // Найти значение максимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <stdio.h>
+long long task2(unsigned A[], size_t n) {
+    if (n < 3) { return -1; }
+    long long max_local_min = -1; 
+    int found_local_min = 0; 
+
+    for (size_t i = 1; i < n - 1; i++) {
+        if (A[i] <= A[i - 1] && A[i] <= A[i + 1]) {
+            found_local_min = 1;
+            if (A[i] > max_local_min) {
+                max_local_min = A[i];
+            }
+        }
+    }
+    return found_local_min ? max_local_min : -1;
 }

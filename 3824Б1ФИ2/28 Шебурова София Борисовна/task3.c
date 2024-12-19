1ed488eaa743090acd4243fc6ff5e7c6442ca6de
sofiasheburova <sofyasheburova@gmail.com>
2024-12-19 10:29:33 +0300

    Update task3.c

--- 3824Б1ФИ2/28 Шебурова София Борисовна/task3.c
+++ 3824Б1ФИ2/28 Шебурова София Борисовна/task3.c
@@ -1,7 +1,26 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // строчных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "aceg".
-char* task3(char *s)
-{
-  return "task3";
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char* task3(char *s) {
+    char *result = (char *)malloc(strlen(s) + 1);
+    if (result == NULL) {
+        return NULL; 
+    }
+
+    int index = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (islower(s[i])) {
+            result[index++] = s[i];
+        }
+    }
+
+    result[index] = '\0';
+
+    return result;
 }

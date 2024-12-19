54b99b21f80b2b6e3770d27c28b4bc31d5bff7de
Алина Скуридина <94843385+Allynall@users.noreply.github.com>
2024-12-19 10:38:53 +0300

    Update task3.c

--- 3824Б1ПР2/21 Скуридина Алина Витальевна/task3.c
+++ 3824Б1ПР2/21 Скуридина Алина Витальевна/task3.c
@@ -1,6 +1,15 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
-char* task3(char *s)
+char task3(char *s)
 {
-  return "task3";
+	int l = 0;
+	int r = strlen(s) - 1;
+	while (l < r) {
+		int d; 
+		char temp = s[l];
+		s[l] = s[r];
+		s[r] = temp;
+		l++; r--;
+	}
+	return s;
 }

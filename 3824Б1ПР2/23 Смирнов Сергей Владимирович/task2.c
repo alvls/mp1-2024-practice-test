563340f68bfa825d974efb100affd3150e6634d2
GrayMatx <sergeysmi54@gmail.com>
2024-12-19 10:05:46 +0300

    Update task2.c

--- 3824Б1ПР2/23 Смирнов Сергей Владимирович/task2.c
+++ 3824Б1ПР2/23 Смирнов Сергей Владимирович/task2.c
@@ -2,11 +2,16 @@
 // Определить количество разных элементов массива.
 size_t task2(double A[], size_t n)
 {
-  int S = 0;
+  int S = 0, is_unique = 1;
   for(int i = 0; i < n; i++) {
-    for(int k = i+1 ; k < n; k++) {
-      if (A[i] != A[k]) { S += 1;}
+    is_unique = 1;
+    for(int k = 0 ; k < i; k++) {
+      if (A[i] == A[k]) {
+        is_unique = 0;
+        break;
+      }
     }
+    if (is_unique) {S+=1;}
   }
   return S;
 }

6bb07ad691600d1efbf8d17a10744bd6c77ea35f
Татьяна <dumpling.lol13@gmail.com>
2024-12-19 09:49:31 +0300

    Update task3.c

--- 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task3.c
+++ 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task3.c
@@ -1,7 +1,32 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, в которой все вхождения символа ch в строку s
 // удвоены. Пример: ch = ‘g’, s = “abgcdgefgk”, ответ: “abggcdggefggk”.
-char* task3(char *s, char ch)
-{
-  return "task3";
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char* s, char ch) {
+    size_t original_length = strlen(s);
+    size_t count = 0;
+    for (size_t i = 0; i < original_length; i++) {
+        if (s[i] == ch) {
+            count++;
+        }
+    }
+
+    size_t new_length = original_length + count; 
+    char* new_string = (char*)malloc(new_length + 1); 
+
+    if (new_string == NULL) {
+        return NULL; 
+    }
+    size_t j = 0; 
+    for (size_t i = 0; i < original_length; i++) {
+        new_string[j++] = s[i]; 
+        if (s[i] == ch) {
+            new_string[j++] = ch; 
+        }
+    }
+
+    new_string[j] = '0'; 
+    return new_string; 
 }

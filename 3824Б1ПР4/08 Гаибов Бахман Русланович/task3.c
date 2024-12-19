d492758633532a9ffe9a9e9e629a0140d87da200
Baahaa2 <bahmon2@gmail.com>
2024-12-19 09:43:18 +0300

    Update task3.c

--- 3824Б1ПР4/08 Гаибов Бахман Русланович/task3.c
+++ 3824Б1ПР4/08 Гаибов Бахман Русланович/task3.c
@@ -1,7 +1,28 @@
-// Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
-// строчных букв английского алфавита, содержащихся в исходной строке s. 
-// Пример: s = "aBc1D2e34F56g", ответ: "aceg".
-char* task3(char *s)
-{
-  return "task3";
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+char* task3(char *s) {
+    int digit_count = 0;
+    for (int i = 0; s[i] != '0'; i++) {
+        if (isdigit(s[i])) {
+            digit_count++;
+        }
+    }
+
+    char *result = (char *)malloc(digit_count + 1);
+    if (result == NULL) {
+        return NULL;
+    }
+
+    int j = 0;
+    for (int i = 0; s[i] != '0'; i++) {
+        if (isdigit(s[i])) {
+            result[j++] = s[i];
+        }
+    }
+
+    result[j] = '0';
+
+    return result;
 }

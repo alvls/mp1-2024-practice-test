b1c4122e743f8d4e089e0d89e0b1be2946054295
Даниил Ципин <daniiltsipin@gmail.com>
2024-12-19 09:51:53 +0300

    Update task2.c

--- 3824Б1ФИ2/27 Ципин Даниил Денисович/task2.c
+++ 3824Б1ФИ2/27 Ципин Даниил Денисович/task2.c
@@ -2,7 +2,30 @@
 // Найти значение минимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <stddef.h>
+#include <limits.h>
+
+long long task2(unsigned A[], size_t n) {
+    if (n < 3) {
+        return -1;
+    }
+
+    unsigned min_local_min = UINT_MAX;
+    int found_local_min = 0;
+
+    for (size_t i = 1; i < n - 1; i++) {
+        if (A[i] <= A[i - 1] && A[i] <= A[i + 1]) {
+            if (A[i] < min_local_min) {
+                min_local_min = A[i]; 
+            }
+            found_local_min = 1;
+        }
+    }
+  
+    if (found_local_min) {
+      return min_local_min;
+    }
+    else {
+      return -1;
+    }
 }

7b58e35d389ef5e47d1835c82fbcb418b60e7a8e
YuriyLichman <river2005supp@gmail.com>
2024-12-19 09:40:49 +0300

    Update task1.c

--- 3824Б1ПР4/14 Личман Юрий Витальевич/task1.c
+++ 3824Б1ПР4/14 Личман Юрий Витальевич/task1.c
@@ -1,7 +1,21 @@
-// Задача 1. Дан непустой массив A из N элементов.
-// Найти номер последнего экстремального (минимального или максимального) 
-// элемента в нем.
-int task1(unsigned A[], size_t N)
-{
-  return -1;
+#include <stdio.h>
+
+int task1(unsigned A[], size_t N) {
+    if (N == 0) {
+        return -1;
+    }
+
+    size_t minIndex = 0;
+    size_t maxIndex = 0;
+
+    for (size_t i = 1; i < N; ++i) {
+        if (A[i] <= A[minIndex]) {
+            minIndex = i;
+        }
+        if (A[i] >= A[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    
+    return (minIndex > maxIndex) ? minIndex : maxIndex;
 }

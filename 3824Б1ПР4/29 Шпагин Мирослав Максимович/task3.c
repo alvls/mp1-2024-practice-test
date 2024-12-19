23cad0bce2a8cda145227aec9996bc88b613fc29
Mirok <58782012+Mirok2u93872957@users.noreply.github.com>
2024-12-19 10:02:45 +0300

    Update task3.c

--- 3824Б1ПР4/29 Шпагин Мирослав Максимович/task3.c
+++ 3824Б1ПР4/29 Шпагин Мирослав Максимович/task3.c
@@ -1,10 +1,19 @@
-// Задача 3. Дана строка s и натуральное число N.
-// Сформировать строку ss длины N следующим образом.
-// Если длина строки s больше или равна N,
-//   то включить в ss N последних символов строки s.
-// Если длина строки s меньше N,
-//   то включить в ss все символы строки s, добавив перед ними символы '*'.
-char* task3(char *s, unsigned N)
-{
-  return "task3";
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char *s, unsigned N) {
+    int dlina = strlen(s);  
+    char* ss;  
+
+    if (dlina >= N) {
+        ss = (char*)malloc(N + 1); 
+        strncpy(ss, s + dlina - N, N); 
+    } else {
+        // Если длина s меньше N
+        ss = (char*)malloc(N + 1); 
+        int star_count = N - dlina; 
+        memset(ss, '*', star_count); 
+        strcpy(ss + star_count, s); 
+    ss[N] = '\\0'; 
+    return ss; 

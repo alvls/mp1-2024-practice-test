87eabd12f94afa45bbf2fa098232376f08dd365d
perf1lev <perfilevivan72@gmail.com>
2024-12-19 10:00:47 +0300

    Update task3.c

--- 3824Б1ФИ3/24 Перфильев Иван Павлович/task3.c
+++ 3824Б1ФИ3/24 Перфильев Иван Павлович/task3.c
@@ -1,6 +1,16 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней строчных букв английского алфавита.
-size_t task3(char *s)
-{
-  return -1;
+#include <stddef.h>
+size_t task3(char* s) {
+    size_t t = 0;
+
+    while (*s) { 
+        if (*s >= 'a' && *s <= 'z') { 
+            t++;
+        }
+        s++; 
+    }
+    if (t == 0) {
+     return -1;
+    return t; 
 }

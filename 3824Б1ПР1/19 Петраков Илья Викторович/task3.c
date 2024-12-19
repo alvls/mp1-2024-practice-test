f4f1d9c38591facec73b4454cdcc559413e71c57
IidleP <i.petrakov52@gmail.com>
2024-12-19 10:20:22 +0300

    Update task3.c

--- 3824Б1ПР1/19 Петраков Илья Викторович/task3.c
+++ 3824Б1ПР1/19 Петраков Илья Викторович/task3.c
@@ -1,7 +1,32 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Скопировать строку s в новую и преобразовать ее так,
 // чтобы все слова в ней начинались с заглавной буквы.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 char* task3(char *s)
 {
-  return "task3";
+  size_t len = strlen(s);
+  char* newStr = (char*)malloc(len + 1);
+  if (!newStr) return NULL;
+  bool newWord_flag = 1;
+  for (size_t i = 0; i < len; ++i) {
+    if (s[i] == ' ') {
+      newStr[i] = s[i];
+      newWord_flag = 1;
+    }
+    else {
+      if (newWord_flag && s[i] >= 'a' && s[i] <= 'z') {
+        newStr[i] = s[i] - ('a' - 'A');
+        newWord_flag = 0;
+      }
+      else {
+        newStr[i] = s[i];
+        newWord_flag = 0;
+      }
+    }
+  }
+  newStr[len] = '\0';
+  return newStr;
+  free(newStr); 
 }

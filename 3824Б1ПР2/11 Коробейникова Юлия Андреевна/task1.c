42425b1cf00dc72243f12324e3b5d0849cc79902
Юлия Коробейникова <yulialklruw@gmail.com>
2024-12-19 10:24:07 +0300

    Update task1.c

--- 3824Б1ПР2/11 Коробейникова Юлия Андреевна/task1.c
+++ 3824Б1ПР2/11 Коробейникова Юлия Андреевна/task1.c
@@ -1,7 +1,17 @@
 // Задача 1. Даны числа a, b (0 < a < b) и непустой массив A из N элементов.
 // Найти минимальный из тех элементов A, которые содержатся в интервале (a, b).
 // Вернуть 0, если требуемые элементы отсутствуют.
+#include <limits.h>
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+  unsigned min_value = 0; // изначально 0, если требуемые элементы отсутствуют
+  for (size_t i = 0; i < n; i++) {
+    if (A[i] > a && A[i] < b) {
+      if (min_value == 0 || A[i] < min_value) {
+        min_value = A[i];
+       }
+    }
+   }
+  return min_value; // вернется 0, если нет требуемых элементов
+// return -1;
 }

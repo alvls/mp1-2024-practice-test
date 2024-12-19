a519c204c5f97c27927f27ee8e1a95419d7f09fd
Никита Бакин 3824Б1ПР4 <127552871+Nikita2356@users.noreply.github.com>
2024-12-19 10:03:34 +0300

    Update task2.c

--- 3824Б1ПР4/03 Бакин Никита Вячеславович/task2.c
+++ 3824Б1ПР4/03 Бакин Никита Вячеславович/task2.c
@@ -1,46 +1,38 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-// Функция для проверки наличия элемента в массиве
-bool isPresent(double arr[], int size, double elem) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == elem) {
-            return true;
-        }
-    }
-    return false;
+int compare(const void *a, const void *b) {
+    // Функция для сравнения двух вещественных чисел
+    if (*(float *)a < *(float *)b) return -1;
+    if (*(float *)a > *(float *)b) return 1;
+    return 0;
 }
 
 int main() {
     int n;
 
-    printf("Введите размер массива: ");
+    printf("Введите количество элементов массива: ");
     scanf("%d", &n);
 
-    if (n <= 0) {
-        printf("Размер массива должен быть больше нуля.\n");
-        return 1;
-    }
+    float A[n]; 
 
-    double A[n];
     printf("Введите элементы массива:\n");
     for (int i = 0; i < n; i++) {
-        scanf("%lf", &A[i]);
+        scanf("%f", &A[i]);
     }
 
-    double unique[n];
-    int uniqueCount = 0;
+    qsort(A, n, sizeof(float), compare);
 
-    for (int i = 0; i < n; i++) {
-        if (!isPresent(unique, uniqueCount, A[i])) {
-            unique[uniqueCount] = A[i];
+    int uniqueCount = 1; // Начинаем с 1, так как первый элемент уникален
+    for (int i = 1; i < n; i++) {
+        if (A[i] != A[i - 1]) {
             uniqueCount++;
         }
     }
 
-    printf("Количество разных элементов массива: %d\n", uniqueCount);
+    printf("Количество различных элементов массива: %d\n", uniqueCount);
 
     return 0;
 }

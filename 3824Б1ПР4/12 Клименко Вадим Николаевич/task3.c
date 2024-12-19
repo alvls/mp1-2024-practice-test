4f4fdab6d6bcc5abb920a8ca5ac76022295ecd73
VadimKlimenko316 <klimvadim05@gmail.com>
2024-12-19 10:10:27 +0300

    Update task3.c

--- 3824Б1ПР4/12 Клименко Вадим Николаевич/task3.c
+++ 3824Б1ПР4/12 Клименко Вадим Николаевич/task3.c
@@ -1,7 +1,34 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, в которой все вхождения символа ch в строку s
 // удвоены. Пример: ch = ‘g’, s = “abgcdgefgk”, ответ: “abggcdggefggk”.
-char* task3(char *s, char ch)
-{
-  return "task3";
+
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char *s, char ch) {
+    if (s == NULL) return NULL;
+
+    size_t len = strlen(s);
+    size_t count = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (s[i] == ch) {
+            count++;
+        }
+    }
+
+    char *new_s = (char *)malloc(len + count + 1);
+    if (new_s == NULL) return NULL;
+
+    size_t j = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (s[i] == ch) {
+            new_s[j++] = ch;
+            new_s[j++] = ch;
+        } else {
+            new_s[j++] = s[i];
+        }
+    }
+    new_s[j] = '\0';
+
+    return new_s;
 }

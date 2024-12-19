0dec55a997c296bb7822fcc965fa8723c4ff6bea
StrelchenkoSofya <sofa.strelchenko05@gmail.com>
2024-12-19 10:39:19 +0300

    Update task3.c

--- 3824Б1ПР1/27 Стрельченко Софья Викторовна/task3.c
+++ 3824Б1ПР1/27 Стрельченко Софья Викторовна/task3.c
@@ -1,7 +1,28 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // заглавных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "BDF".
-char* task3(char *s)
+#include<malloc.h>
+char* task3(char* s)
 {
-  return "task3";
+	int l = 0, l2 = 0;
+	char* s2;
+	while (s[l] != '\0') {
+		if ((s[l] >= 'A') && (s[l] <= 'Z')) {
+			l2++;
+		}
+		l++;
+	}
+	s2 = (char*)malloc(sizeof(char) * (l2 + 1));
+	l = 0;
+	l2 = 0;
+	while (s[l] != '\0') {
+		if ((s[l] >= 'A') && (s[l] <= 'Z')) {
+			s2[l2]=s[l];
+      l2++;
+		}
+		l++;
+	}
+	return s2;
+	char t[5] = { 't','a','s','k','3' };
+	return t;
 }

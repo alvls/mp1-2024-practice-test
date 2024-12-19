98f763905db6bcdd97eecda9e6c654e5a7e8a4a7
IidleP <i.petrakov52@gmail.com>
2024-12-19 09:56:13 +0300

    Update task2.c

--- 3824Б1ПР1/19 Петраков Илья Викторович/task2.c
+++ 3824Б1ПР1/19 Петраков Илья Викторович/task2.c
@@ -2,7 +2,21 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 2).
 // Найти число серий длины 2 в массиве.
+#include <stdio.h>
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+  int count = 0;
+  for (size_t i = 0; i < n - 1; ++i) {
+    if (A[i] == A[i + 1]) {
+      int series_length = 1;
+      while (i < n - 1 && A[i] == A[i + 1]) {
+        series_length++;
+        i++;
+      }
+      if (series_length == 2) {
+        count++;
+      }
+    }
+  }
+return count;
 }

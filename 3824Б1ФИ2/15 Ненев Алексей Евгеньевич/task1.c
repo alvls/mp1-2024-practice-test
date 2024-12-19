9cacfa70504415b0e4263ba42421bc4d4f7cce7f
para5ol <an02.02@mail.ru>
2024-12-19 09:42:23 +0300

    Update task1.c

--- 3824Б1ФИ2/15 Ненев Алексей Евгеньевич/task1.c
+++ 3824Б1ФИ2/15 Ненев Алексей Евгеньевич/task1.c
@@ -3,5 +3,22 @@
 // и последним максимальным элементами.
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+  unsigned minIndex = 0, maxIndex = 0;
+  unsigned minValue = A[0], maxValue = A[0];
+  for (unsigned i = 1; i < N; i++) {
+      if (A[i] < minValue) {
+          minValue = A[i];
+          minIndex = i;
+      }
+      if (A[i] >= maxValue) { 
+          maxValue = A[i];
+          maxIndex = i;
+      }
+  }
+  if (minIndex > maxIndex) {
+      unsigned tmp = minIndex;
+      minIndex = maxIndex;
+      maxIndex = tmp;
+  }
+  return maxIndex - minIndex - 1;
 }

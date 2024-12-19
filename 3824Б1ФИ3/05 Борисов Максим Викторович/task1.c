068e3b61ecc44c9ad2ac1b531ef819cbf48a5d63
MaksimBorisovFI3 <morgenhto@gmail.com>
2024-12-19 09:32:57 +0300

    Update task1.c

--- 3824Б1ФИ3/05 Борисов Максим Викторович/task1.c
+++ 3824Б1ФИ3/05 Борисов Максим Викторович/task1.c
@@ -2,5 +2,15 @@
 // Найти сколько чисел в массиве меньше своего левого соседа.
 size_t task1(int A[], size_t n)
 {
+  if (n < 2) {
+        return 0;
+    }
+    size_t count = 0; 
+    for (size_t i = 1; i < n; i++) { 
+        if (A[i] < A[i - 1]) { 
+            count++; 
+        }
+    }
+    return count;
   return -1;
 }

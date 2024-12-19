91bed45e224b945f61ce40c7505cebbfddabaa15
Daemos17 <89529780+Daemos17@users.noreply.github.com>
2024-12-19 10:22:06 +0300

    Update task1.c

--- 3824Б1ПР4/10 Казанцев Даниил Алексеевич/task1.c
+++ 3824Б1ПР4/10 Казанцев Даниил Алексеевич/task1.c
@@ -1,6 +1,15 @@
-// Задача 1. Дан непустой массив A, содержащий n вещественных чисел.
+// Задача 1. Дан непустой массив A, содержащий n вещественных??? чисел.
 // Найти номер его последнего максимального элемента.
 int task1(int A[], size_t n)
-{
-  return -1;
+{  
+    int last_max_index = 0;
+    int max_value = A[0];
+
+    for (int i = 1; i < n; i++) {
+        if (A[i] >= max_value) {
+            max_value = A[i];
+            last_max_index = i;
+        }
+    }
+    return last_max_index;
 }

148688bf9a7278a69ec472ae4b46514ed2cfda02
rovchi <rodinegor03072006@gmail.com>
2024-12-19 10:21:58 +0300

    Update task3.c

--- 3824Б1ПР3/18 Родин Егор Алексеевич/task3.c
+++ 3824Б1ПР3/18 Родин Егор Алексеевич/task3.c
@@ -1,6 +1,24 @@
-// Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
-// (одним или несколькими). Определить длину самого короткого слова в строке.
-int task3(char *s)
+#include <ctype.h>
+int task3(char* s)
 {
-  return -1;
+    int min_length = -1; //переменная для хранения длины самого короткого слова
+    int current_length = 0; //текущая длина слова
+
+    while (*s)
+    {
+        if (!isspace(*s)) //если текущий символ не пробел
+        {
+            current_length++; //увеличиваем длину текущего слова
+        }
+        else if (current_length > 0) 
+        {
+            if (min_length == -1 || current_length < min_length)
+                min_length = current_length; //обновляем минимальную длину
+            current_length = 0;
+        }
+        s++;
+    }
+    if (current_length > 0 && (min_length == -1 || current_length < min_length)) //проверка на последнее слово
+        min_length = current_length;
+    return min_length;
 }

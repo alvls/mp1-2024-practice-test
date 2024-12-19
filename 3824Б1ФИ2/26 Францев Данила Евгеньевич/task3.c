95c662c8c9b6cc02230099210cf1137e3db7e05f
DaniFrantz <gigabait24@mail.ru>
2024-12-19 10:35:13 +0300

    Update task3.c

--- 3824Б1ФИ2/26 Францев Данила Евгеньевич/task3.c
+++ 3824Б1ФИ2/26 Францев Данила Евгеньевич/task3.c
@@ -1,6 +1,8 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из цифр,
 // содержащихся в исходной строке s.
 // Пример: s = "abc1d2e34f56g", ответ: "123456".
+
+#include <stdlib.h>
 char* task3(char* s)
 {
     char* c;
@@ -12,15 +14,18 @@ char* task3(char* s)
         }
         beb += 1;
     }
-    c = (char*)malloc(schet-4);
+    c = (char*)calloc(schet, 2);
+    int dild = beb;
     beb = 0;
     schet = 0;
-    while (s[beb] != '\0') {
-        if (s[beb] > 47 && s[beb] < 58) {
-            c[schet] = s[beb];
-            schet += 1;
+    if (c) {
+        while (beb != dild) {
+            if (s[beb] > 47 && s[beb] < 58) {
+                c[schet] = s[beb];
+                schet += 1;
+            }
+            beb += 1;
         }
-        beb += 1;
     }
     return c;
 }

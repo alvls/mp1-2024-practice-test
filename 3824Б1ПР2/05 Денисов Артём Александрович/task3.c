98afb87dfaf2717c376baeb7d7ab93fd4599e48d
_Mindal <79373184+Mindally@users.noreply.github.com>
2024-12-19 10:25:48 +0300

    Update task3.c

--- 3824Б1ПР2/05 Денисов Артём Александрович/task3.c
+++ 3824Б1ПР2/05 Денисов Артём Александрович/task3.c
@@ -2,5 +2,28 @@
 // языка С, перевести число в строковое представление в десятичном виде.
 char* task3(int N)
 {
-  return "task3";
+    int index = 0, isNegative = 0;
+    char res[1000000];
+
+    if (N < 0) {
+        isNegative = 1;
+        N = (-1) * N;
+    }
+    if (N == 0) res[index++] = '0';
+    while (N > 0) {
+        res[index++] = (N % 10) + '0';
+        N /= 10;
+    }
+    if (isNegative) res[index++] = '-';
+    res[index] = '\0';
+    int start = 0;
+    int end = index - 1;
+    while (start < end) {
+        char temp = res[start];
+        res[start] = res[end];
+        res[end] = temp;
+        start++;
+        end--;
+    }
+    return res;
 }

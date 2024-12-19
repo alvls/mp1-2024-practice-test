29a9fa26dfe71c1ef4f531f381e6dfb980d49458
StrogovaNadezhda <Strogova02@inbox.ru>
2024-12-19 09:55:46 +0300

    Update task1.c

--- 3824Б1ПР4/22 Строгова Надежда Александровна/task1.c
+++ 3824Б1ПР4/22 Строгова Надежда Александровна/task1.c
@@ -3,5 +3,21 @@
 // иначе вернуть 0.
 int task1(unsigned A[], size_t n)
 {
+  int task1(unsigned A[], size_t n) {
+    if (n < 2) {
+        return 1;
+    }
+
+    int difference = A[1] - A[0];
+
+    for (size_t i = 2; i < n; i++) {
+        if (A[i] - A[i - 1] != difference) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
   return -1;
 }

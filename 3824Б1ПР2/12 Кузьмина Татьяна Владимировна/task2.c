94cbb1376ab0a2549b1df5562445a9ff2cd612eb
Татьяна <dumpling.lol13@gmail.com>
2024-12-19 09:33:42 +0300

    Update task2.c

--- 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task2.c
+++ 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task2.c
@@ -2,5 +2,14 @@
 // Заменить в нем все элементы, встречающиеся ровно два раза, на -1.
 void task2(int A[], size_t n)
 {
-  A[0] = 1111111111;
+int* count = (int*)calloc(1000000, sizeof(int)); 
+for (size_t i = 0; i < n; i++) {
+    count[A[i]]++;
+}
+for (size_t i = 0; i < n; i++) {
+    if (count[A[i]] == 2) {
+        A[i] = -1;
+    }
+}
+free(count);
 }

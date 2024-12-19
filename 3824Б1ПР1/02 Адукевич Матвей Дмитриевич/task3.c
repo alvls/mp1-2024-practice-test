147f17e73376f2a81573a2201ac45e74ea5aef45
Matvey-Adukevich <matvey.adukevich@gmail.com>
2024-12-19 10:06:10 +0300

    Update task3.c

--- 3824Б1ПР1/02 Адукевич Матвей Дмитриевич/task3.c
+++ 3824Б1ПР1/02 Адукевич Матвей Дмитриевич/task3.c
@@ -1,5 +1,13 @@
 // Задача 3. Дана строка s. Подсчитать количество содержащихся в ней цифр.
-size_t task3(char *s)
+#include <string.h>
+size_t task3(char* s)
 {
-  return -1;
+	int a = strlen(s);
+	int k = 0;
+	for (int i = 0; i < a; i++) {
+		if ((s[i] >= '0') && (s[i] <= '9')) {
+			k++;
+		}
+	}
+	return k;
 }

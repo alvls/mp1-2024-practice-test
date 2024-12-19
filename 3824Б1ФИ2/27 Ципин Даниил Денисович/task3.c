3ceb6cdf6113e883336fa073a97265083e7515bb
Даниил Ципин <daniiltsipin@gmail.com>
2024-12-19 10:23:08 +0300

    Update task3.c

--- 3824Б1ФИ2/27 Ципин Даниил Денисович/task3.c
+++ 3824Б1ФИ2/27 Ципин Даниил Денисович/task3.c
@@ -1,7 +1,27 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // заглавных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "BDF".
-char* task3(char *s)
-{
-  return "task3";
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char *s) {
+    int len = strlen(s);
+    
+    char *result = (char *)malloc(len + 1);
+    if (result == NULL) {
+        return NULL;
+    }
+
+    int j = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            result[j++] = s[i];
+        }
+    }
+
+    result[j] = '\0';
+
+    return result;
 }

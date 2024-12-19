7bc66da0bd9bc6c0973bba9e820198f4d9ff3f8b
кейт-шип-ит <ep2394218@gmail.com>
2024-12-19 10:28:54 +0300

    Екатерина Петрова Вячеславовна 3824Б1ПР1

--- 3824Б1ПР1/20 Петрова Екатерина Вячеславовна/task3.c
+++ 3824Б1ПР1/20 Петрова Екатерина Вячеславовна/task3.c
@@ -2,7 +2,40 @@
 // (одним или несколькими). Скопировать строку s в новую и зашифровать ее,
 // заменяя каждую букву на следующую за ней в алфавите 
 // (считать, что за ‘z’ следует ‘a’), сохраняя регистр.
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+#include<ctype.h>
+
 char* task3(char *s)
 {
-  return "task3";
+    char* tmp = (char*)malloc(strlen(s) + 1);
+    if (tmp == NULL)
+        return NULL;
+
+    for (size_t i = 0; s[i]; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            if (s[i] == 'z') {
+                tmp[i] = 'a';
+            }
+            else {
+                tmp[i] = s[i] + 1;
+            }
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            if (s[i] == 'Z') {
+                tmp[i] = 'A';
+            }
+            else {
+                tmp[i] = s[i] + 1;
+            }
+        }
+        else 
+        {
+            tmp[i] = s[i];
+        }
+    }
+    tmp[strlen(s)] = '\0';
+
+    return tmp;
 }

d086c40bf1375c1356796074384d7683ed4d4cf9
DaniFrantz <gigabait24@mail.ru>
2024-12-19 09:12:30 +0300

    Update task1.c

--- 3824Б1ФИ2/26 Францев Данила Евгеньевич/task1.c
+++ 3824Б1ФИ2/26 Францев Данила Евгеньевич/task1.c
@@ -2,5 +2,9 @@
 // Найти сколько чисел в массиве меньше своего правого соседа.
 size_t task1(int A[], size_t n)
 {
-  return -1;
+    int kolokol = 0;
+    for (int j = 1; j < n; j++) {
+        if (A[j] > A[j - 1]) { kolokol += 1; }
+    }
+    return kolokol;
 }

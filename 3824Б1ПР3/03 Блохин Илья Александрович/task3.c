ae8dc3a49ae99aeeed9edd202ece6e187aaa6c6d
IntFxZen <86683282+IntFxZen@users.noreply.github.com>
2024-12-19 09:31:54 +0300

    Update task3.c

--- 3824Б1ПР3/03 Блохин Илья Александрович/task3.c
+++ 3824Б1ПР3/03 Блохин Илья Александрович/task3.c
@@ -1,6 +1,14 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
+#include <stddef.h>
 size_t task3(char *s)
 {
-  return -1;
+      size_t count = 0;
+    while (*s) {
+        if (*s >= 'A' && *s <= 'Z') {
+            count++;
+        }
+        s++;
+    }
+    return count;
 }

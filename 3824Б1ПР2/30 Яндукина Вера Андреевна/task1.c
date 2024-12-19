f46b8cd6bccd9713b261ea43221fabbe22ba846f
Vera <verayandukina@yandex.ru>
2024-12-19 09:44:51 +0300

    Update task1.c

--- 3824Б1ПР2/30 Яндукина Вера Андреевна/task1.c
+++ 3824Б1ПР2/30 Яндукина Вера Андреевна/task1.c
@@ -1,6 +1,17 @@
 // Задача 1. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти номер его последнего максимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <limits.h>
+int task1(int A[], size_t n) {
+    int num = -1; 
+    int max_elem = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (A[i] > max_elem) {
+            max_elem = A[i];
+            num = i; 
+        }
+        else if (A[i] == max_elem) {
+            num = i; 
+        }
+    }
+    return num;
 }

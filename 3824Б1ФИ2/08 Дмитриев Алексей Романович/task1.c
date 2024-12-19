2623399437b7f5358a9bf22196065f2addc01b55
AdLeGeR <dmitr1effal@yandex.ru>
2024-12-19 10:30:49 +0300

    done

--- 3824Б1ФИ2/08 Дмитриев Алексей Романович/task1.c
+++ 3824Б1ФИ2/08 Дмитриев Алексей Романович/task1.c
@@ -1,7 +1,25 @@
+#include <stddef.h>
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между последними 
 // двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1111111111;
+    long long int cur_sum = 0;
+    long long int total_sum = 0;
+    int flag1 = 0;
+    for(size_t i = 0; i < n; i++){
+        if(A[i] == 0 && flag1 == 0){
+            flag1 = 1;
+            continue;
+        }
+        if(A[i] == 0){
+            cur_sum = 0;
+        }
+        if(flag1){
+            cur_sum += A[i];
+            total_sum += A[i];
+        }
+    }
+    total_sum -= cur_sum;
+    return total_sum;
 }

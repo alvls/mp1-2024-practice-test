88350004e567a668a2e58756ef34d46a9ce7c57f
Egor-Lebedev18 <egorl7801@gmail.com>
2024-12-19 09:51:07 +0300

    Update task2.c

--- 3824Б1ПР6/15 Лебедев Егор Алексеевич/task2.c
+++ 3824Б1ПР6/15 Лебедев Егор Алексеевич/task2.c
@@ -3,6 +3,15 @@
 // а длиной серии – количество этих элементов (длина серии может быть равна 1).
 // Найти число серий в массиве.
 int task2(unsigned A[], size_t n)
-{
-  return -1;
+ if (n == 0) return 0; 
+
+    int series_count = 1; 
+
+    for (size_t i = 1; i < n; ++i) {
+        if (A[i] != A[i - 1]) {
+            ++series_count;
+        }
+    }
+
+    return series_count;
 }

a76459fe011b0ffe98ccf535c6bcdb83ae05e7cb
Grushet187 <maxgrush2006@gmail.com>
2024-12-19 09:17:11 +0300

    Update task1.c

--- 3824Б1ФИ1/10 Грушецкий Максим Анатольевич/task1.c
+++ 3824Б1ФИ1/10 Грушецкий Максим Анатольевич/task1.c
@@ -2,5 +2,14 @@
 // Найти номер его последнего максимального элемента.
 int task1(int A[], size_t n)
 {
+  int n = 0;
+int ind = 0;
+for (int i = 0; i < n; i++)
+{
+	if (A[i] >= n)
+	{
+		n = A[i];
+		ind = i;
+	}
   return -1;
 }

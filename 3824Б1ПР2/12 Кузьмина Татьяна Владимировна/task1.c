a7badc2aa445c733f8e5b12f0789194ebddcb320
Татьяна <dumpling.lol13@gmail.com>
2024-12-19 09:44:07 +0300

    Update task1.c

--- 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task1.c
+++ 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task1.c
@@ -2,19 +2,20 @@
 // Найти максимальный из тех элементов A, которые содержатся в интервале (a, b).
 // Вернуть 0, если требуемые элементы отсутствуют.
 #include <limits.h>
+#include <stdlib.h>
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-    int max = 0; 
+    unsigned max_value = 0; 
     int found = 0; 
 
     for (size_t i = 0; i < n; i++) {
         if (A[i] > a && A[i] < b) { 
-            if (!found || A[i] > max) { 
-                max = A[i];
+            if (!found || A[i] > max_value) { 
+                max_value = A[i];
                 found = 1; 
             }
         }
     }
 
-    return found ? max : 0; 
+    return found ? max_value : 0;
 }

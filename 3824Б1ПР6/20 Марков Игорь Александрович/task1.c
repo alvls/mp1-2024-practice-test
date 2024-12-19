fa77a738661649d58c37e355428134e1b01bde89
markovjr <gosha200608@gmail.com>
2024-12-19 09:40:47 +0300

    Update task1.c

--- 3824Б1ПР6/20 Марков Игорь Александрович/task1.c
+++ 3824Б1ПР6/20 Марков Игорь Александрович/task1.c
@@ -1,7 +1,17 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Преобразовать его, прибавив к нечетным числам первый элемент,
 // а к четным – последний. Первый и последний элементы массива не менять. 
-void task1(unsigned A[], size_t N)
-{
-  A[0] = 1111111111;
+void task1(unsigned A[], size_t N) {
+    if (N <= 2) {
+        return;
+    }
+    unsigned firstElement = A[0];
+    unsigned lastElement = A[N - 1];
+    for (size_t i = 1; i < N - 1; ++i) {
+        if (A[i] % 2 == 0) {
+            A[i] += lastElement;
+        } else {
+            A[i] += firstElement;
+        }
+    }
 }

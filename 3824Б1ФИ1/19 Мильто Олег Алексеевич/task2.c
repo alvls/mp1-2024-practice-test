c591229f5bb6a5dfc758aef0d024c4f5d5367efc
img-gif <Olegmilto15@gmail.com>
2024-12-19 10:16:21 +0300

    Update task2.c

--- 3824Б1ФИ1/19 Мильто Олег Алексеевич/task2.c
+++ 3824Б1ФИ1/19 Мильто Олег Алексеевич/task2.c
@@ -2,7 +2,39 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 2).
 // Найти число серий длины 2 в массиве.
+#include <stdio.h>
+#include <stdlib.h>
+
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+  if (n < 2)
+  {
+    return 0;
+  }
+
+  int count = 0;
+  int res = 0;
+  for (int i = 0; i < n - 1; ++i)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      if ((A[i] == A[j]))
+        count++;
+      else
+      {
+        if (count != 1)
+        {
+          count = 0;
+          i = j;
+        }
+        else
+        {
+          res += 1;
+          count = 0;
+        }
+      }
+    }
+  }
+
+  return res;
 }

8e2e8782389b8f27ac818a87dbb7d1e2ce2687e3
fafaf6 <lukasovdanil2@gmail.com>
2024-12-19 09:45:26 +0300

    Update task1.c

--- 3824Б1ПР3/10 Лукашов Данила Андреевич/task1.c
+++ 3824Б1ПР3/10 Лукашов Данила Андреевич/task1.c
@@ -1,6 +1,22 @@
 // Задача 1. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти номер его последнего максимального элемента.
+#include <stdio.h>
 int task1(int A[], size_t n)
 {
-  return -1;
+  if (n <= 0)
+  {
+return -1;
+  }
+  int max = 0;
+  int index = 0;
+  for (int i = 0; i < n; i++)
+    {
+    if ( A[i) > max)
+    {
+      max = A[i];
+      index = i;
+    }
+      
+    }
+  return index;
 }

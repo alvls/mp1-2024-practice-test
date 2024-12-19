c0631178c71bdd2f009e1d9ba2aac1be9e90e0d7
Mercurial00 <148081131+Mercurial00@users.noreply.github.com>
2024-12-19 10:16:30 +0300

    Update task3.c

--- 3824Б1ФИ2/23 Старостин Данил Денисович/task3.c
+++ 3824Б1ФИ2/23 Старостин Данил Денисович/task3.c
@@ -5,7 +5,7 @@
 #include <string.h>
 size_t task3(char* s)
 {
-	int res = 0;
+	size_t res = 0;
 	size_t size = strlen(s);
 	for (size_t i = 0; i < size; ++i) {
 		if (isalpha(s[i]) && isupper(s[i])) {

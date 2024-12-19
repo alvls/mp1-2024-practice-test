cda30449a09011a49d7a74a6cab597870d98efec
vladB77-name <vladislav.student.08@mail.ru>
2024-12-19 10:37:42 +0300

    Update task3.c

--- 3824Б1ФИ2/03 Большаков Владислав Владимирович/task3.c
+++ 3824Б1ФИ2/03 Большаков Владислав Владимирович/task3.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+#include <string.h>
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
-size_t task3(char *s)
+size_t task3(char* s)
 {
-  return -1;
+	
+	size_t count = 0; 
+	for (size_t i = 0; i < strlen(s); i++) {
+		if (s[i]>='A' && s[i] <='Z') {
+			count++;
+		}
+	} 
+	return count; 
 }

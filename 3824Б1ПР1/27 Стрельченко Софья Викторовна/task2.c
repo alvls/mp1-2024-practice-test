acc20bf22ff627542d4c2dc109f95195dec5160e
StrelchenkoSofya <sofa.strelchenko05@gmail.com>
2024-12-19 09:45:52 +0300

    Update task2.c

--- 3824Б1ПР1/27 Стрельченко Софья Викторовна/task2.c
+++ 3824Б1ПР1/27 Стрельченко Софья Викторовна/task2.c
@@ -4,5 +4,19 @@
 // При отсутствии локальных минимумов вернуть -1.
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+	long long minlm = LLONG_MAX;
+	for (int i = 1; i < n - 1; i++) {
+		if ((A[i] <= A[i - 1]) && (A[i] <= A[i + 1])) {
+			if (A[i] < minlm) {
+				minlm = A[i];
+			}
+		}
+	}
+	if (minlm == LLONG_MAX) {
+		return -1;
+	}
+	else {
+		return minlm;
+	}
+	return -2;
 }

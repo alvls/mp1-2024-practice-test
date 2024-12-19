f2c99e7bbff1e17d80b83c2444ef17afd4b774e4
BLACK_72 <zoooblack7@gmail.com>
2024-12-19 09:34:57 +0300

    Update task1.c

--- 3824Б1ФИ1/04 Вайло Дмитрий Сергеевич/task1.c
+++ 3824Б1ФИ1/04 Вайло Дмитрий Сергеевич/task1.c
@@ -3,5 +3,27 @@
 // иначе вернуть 0.
 int task1(double A[], size_t n)
 {
-  return -1;
+  if (n < 2) {
+    return 1;
+}
+
+if (A[0] == 0) {
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] != 0) {
+            return 0;
+        }
+    }
+    return 1; 
+}
+
+double rat = A[1] / A[0];
+
+
+for (size_t i = 2; i < n; i++) {
+    if (A[i - 1] == 0 || A[i] / A[i - 1] != rat) {
+        return 0; 
+    }
+}
+
+  return 1;
 }

d1a1f75e359c8e1f294cbf28b7726a41ececc0b4
Polina0013 <123825611+Polina0013@users.noreply.github.com>
2024-12-19 09:47:07 +0300

    Update task1.c

--- 3824Б1ПР2/18 Позднова Полина Дмитриевна/task1.c
+++ 3824Б1ПР2/18 Позднова Полина Дмитриевна/task1.c
@@ -1,8 +1,19 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Сформировать новый массив, в котором вначале расположены элементы массива A
 // с четными индексами, а затем – с нечетными. 
+
+#include <stdlib.h>
+
 int* task1(int A[], size_t N)
 {
-  A[0] = 1111111111;
-  return A;
+int* newA = (int*)malloc(N * sizeof(int));
+size_t evenidx = 0;
+size_t oddidx = (N + 1) / 2;
+for (size_t i = 0; i < N; i += 2) {
+    newA[evenidx++] = A[i];
+}
+for (size_t i = 1; i < N; i += 2) {
+    newA[oddidx++] = A[i];
+}
+return newA;
 }

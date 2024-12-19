4abbc9bbd61d5ae4389c1a915fa8eae87c67be65
alex00turbo <alex2006turbo@gmail.com>
2024-12-19 10:28:26 +0300

    Arisov Alexandr_2.c

--- 3824Б1ПР5/01 Арисов Александр Сергеевич/task2.c
+++ 3824Б1ПР5/01 Арисов Александр Сергеевич/task2.c
@@ -1,15 +1,31 @@
-// Задача 2. Даны: 1) непустой массив A, содержащий n целых чисел, 
-// 2) вещественное число R.
-// Найти индексы двух элементов массива, сумма которых наиболее близка к числу R.
-// Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
-// При невозможности найти пару, вернуть {-1,-1}.
-struct Indices
-{
+#include <math.h>
+#include <float.h>
+#include <stddef.h>
+
+struct Indices {
   int first_index;
   int second_index;
 };
-struct Indices task2(int A[], size_t n, double R)
-{
-  struct Indices indices = {-2, -2};
+
+struct Indices task2(int A[], size_t n, double R) {
+  struct Indices indices = {-1, -1};  
+  double closest_diff = DBL_MAX; 
+  if (n < 2) {
+    return indices;
+  }
+
+  for (size_t i = 0; i < n - 1; i++) {
+    for (size_t j = i + 1; j < n; j++) {
+      double current_sum = A[i] + A[j];
+      double current_diff = fabs(current_sum - R);
+      if (current_diff < closest_diff ||
+          (current_diff == closest_diff && (i < indices.first_index || (i == indices.first_index && j < indices.second_index)))) {
+        indices.first_index = i;
+        indices.second_index = j;
+        closest_diff = current_diff;
+          }
+    }
+  }
+
   return indices;
 }

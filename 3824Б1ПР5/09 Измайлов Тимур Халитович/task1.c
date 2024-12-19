4bade50f1053eafcd4d197521b4980d486bb3a6b
Raisl <69871465+Raisl@users.noreply.github.com>
2024-12-19 09:28:18 +0300

    Измайлов пр5 Update task1.c

--- 3824Б1ПР5/09 Измайлов Тимур Халитович/task1.c
+++ 3824Б1ПР5/09 Измайлов Тимур Халитович/task1.c
@@ -1,6 +1,15 @@
-// Задача 1. Дан непустой массив A, содержащий n целых чисел.
-// Найти номер его последнего минимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <stddef.h>
+
+size_t task1(int A[], size_t n) {
+    if (n == 0) {
+        return (size_t)-1; 
+    }
+
+    size_t minIndex = 0;
+    for (size_t i = 1; i < n; ++i) {
+        if (A[i] <= A[minIndex]) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
 }

0d6718c06c46cce21d2c4611b4952fce0001d014
Vorobyov05 <alekseevaekaterina506@gmail.com>
2024-12-19 09:23:16 +0300

    Update task1.c

--- 3824Б1ФИ3/07 Воробьев Дмитрий Сергеевич/task1.c
+++ 3824Б1ФИ3/07 Воробьев Дмитрий Сергеевич/task1.c
@@ -3,5 +3,32 @@
 // двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1;
+	int first_zero_ind = -1;
+	int second_zero_ind = -1;
+	int sum = 0;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		if (A[i] == 0)
+		{
+			if (first_zero_ind == -1)
+			{
+				first_zero_ind = i;
+			}
+			else
+			{
+				second_zero_ind = i;
+				break;
+			}
+		}
+	}
+	if (second_zero_ind == -1)
+	{
+		return -1;
+	}
+	for (size_t i = first_zero_ind + 1; i < second_zero_ind; i++)
+	{
+		sum += A[i];
+	}
+	return sum;
 }

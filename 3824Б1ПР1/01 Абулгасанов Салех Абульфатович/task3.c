9ff40bdb31d86da1af8487459b456d23ade4e9d7
Salekh-A <abulgasanovsaleh1@gmail.com>
2024-12-19 10:35:43 +0300

    Update task3.c

--- 3824Б1ПР1/01 Абулгасанов Салех Абульфатович/task3.c
+++ 3824Б1ПР1/01 Абулгасанов Салех Абульфатович/task3.c
@@ -2,9 +2,9 @@
 #include <string.h>
 char* task3(char* s)
 {
-    int len = strlen(s);
+    int len = strlen(s), j = 0;;
     char* obr = (char*)malloc((len + 1) * sizeof(char));
-    for (int i = 0; i < len; i++) obr[i] = s[len - 1 - i];
+    for (int i = len - 1; i >= 0; i--) obr[j++] = s[i];
     obr[len] = '\0';
-    return obr; 
+    return obr;
 }

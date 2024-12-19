bbd99a5cdeab1278838163e6a0bf80f20282dd03
pupupu <fedyninaarina2000@gmail.com>
2024-12-19 09:47:19 +0300

    Update task1.c

--- 3824Б1ФИ3/27 Федянина Арина Дмитриевна/task1.c
+++ 3824Б1ФИ3/27 Федянина Арина Дмитриевна/task1.c
@@ -3,5 +3,50 @@
 // двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1;
+    int zero1 = -1;
+    int zero2 = -1;
+    
+    if (n < 2) 
+    {
+        return 0;
+    }
+
+  
+    for (int i = 0; i < n; i++) 
+    {
+        if (A[i] == 0) 
+        {
+            if (zero1 == -1) 
+            {
+                zero1 = i;
+            }
+            else if (zero2 == -1) 
+            {
+                zero2 = i;
+                break; 
+            }
+        }
+    }
+
+  
+    if (zero1 == -1 || zero2 == -1) 
+    {
+        return 0;
+    }
+
+
+    if (zero2 - zero1 <= 1) 
+    {
+        return 0;
+    }
+
+
+    int sum = 0;
+    for (int i = zero1 + 1; i < zero2; i++) 
+    {
+        sum += A[i];
+    }
+
+    return sum;
+}
 }

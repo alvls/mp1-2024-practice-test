1557afc307f1d6e2b720c23670eb1e70b6908bf4
doker69 <dani.rasskazov1@gmail.com>
2024-12-19 10:25:16 +0300

    Рассказов Данила ПР3824Б1ПР5 task1.c

--- 3824Б1ПР5/18 Рассказов Данила Сергеевич/task1.c
+++ 3824Б1ПР5/18 Рассказов Данила Сергеевич/task1.c
@@ -1,8 +1,29 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Сформировать новый массив, в котором вначале расположены элементы массива A
 // с четными индексами, а затем – с нечетными. 
-int* task1(int A[], size_t N)
-{
-  A[0] = 1111111111;
-  return A;
+#include <stdio.h>
+#include <stdlib.h>
+
+int* task1(const int A[], size_t N) {
+    if (A == NULL || N == 0) {
+        return NULL;
+    }
+
+    int new_array = (int)malloc(N * sizeof(int));
+    if (new_array == NULL) {
+        fprintf(stderr, "Ошибка выделения памяти.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t new_index = 0;
+
+    for (size_t i = 0; i < N; i += 2) {
+        new_array[new_index++] = A[i];
+    }
+
+    for (size_t i = 1; i < N; i += 2) {
+        new_array[new_index++] = A[i];
+    }
+
+    return new_array;
 }

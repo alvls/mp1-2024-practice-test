6398331af39dd54fbd2408d6d2a42accd780c6e4
Elisey Boyko <116101705+NiGrammaMorali@users.noreply.github.com>
2024-12-19 09:23:25 +0300

    Update task1.c

--- 3824Б1ФИ2/02 Бойко Елисей Иванович/task1.c
+++ 3824Б1ФИ2/02 Бойко Елисей Иванович/task1.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
+
 // Задача 1. Дан непустой массив A, содержащий n натуральных чисел.
 // Вернуть 1, если его элементы образуют арифметическую прогрессию,
 // иначе вернуть 0.
 int task1(unsigned A[], size_t n)
 {
-  return -1;
+	int c = A[1] - A[0];
+	int flag = 1;
+	for (int i = 1; i < n; i++) {
+		if (A[i] == A[i-1] + c) {
+			continue;
+		}
+		else {
+			flag = 0;
+			break;
+		}
+	}
+	if (flag) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }

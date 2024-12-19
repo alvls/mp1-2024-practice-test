0a9b023a9fc20b9ddb19354d3e3d10f3c65a5bc3
Frestelse <89030541016@mail.ru>
2024-12-19 09:29:47 +0300

    Update task1.c

--- 3824Б1ФИ2/29 Щепетильникова Мария Александровна/task1.c
+++ 3824Б1ФИ2/29 Щепетильникова Мария Александровна/task1.c
@@ -1,6 +1,20 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+int task1(int A[], size_t n){
+    if (n == 0) { // Проверка на пустой массив (хотя по условию он непустой)
+        return -1; 
+    }
+    int min_index = -1; 
+    int min_value = A[0]; 
+
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] < min_value) {
+            min_value = A[i]; 
+            min_index = i; 
+        }
+        else if (A[i] == min_value) {
+            min_index = i; 
+        }
+    }
+    return min_index;
 }

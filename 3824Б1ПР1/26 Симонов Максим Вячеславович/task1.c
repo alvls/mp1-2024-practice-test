8c5785b0d33b4a2a3705e4346edab10558dc9b28
Максим Симонов 3824Б1ПР1 <116003845+UuAcC@users.noreply.github.com>
2024-12-19 09:12:03 +0300

    Update task1.c

--- 3824Б1ПР1/26 Симонов Максим Вячеславович/task1.c
+++ 3824Б1ПР1/26 Симонов Максим Вячеславович/task1.c
@@ -2,5 +2,7 @@
 // Найти сколько чисел в массиве меньше своего правого соседа.
 size_t task1(int A[], size_t n)
 {
-  return -1;
+  n--; int c = 0;
+  for (int i = 0;i < n;i++) { if (arr[i] < arr[i + 1])c++; }
+  return c;
 }

4187a2cd1e78f82596a7f0ce09e8399437f5143a
Alexandr-Berezin-art <berezin.aleksandr.186@gmail.com>
2024-12-19 09:50:36 +0300

    Update task3.c

--- 3824Б1ПР1/04 Березин Александр Анатольевич/task3.c
+++ 3824Б1ПР1/04 Березин Александр Анатольевич/task3.c
@@ -2,5 +2,11 @@
 // Подсчитать количество содержащихся в ней строчных букв английского алфавита.
 size_t task3(char *s)
 {
-  return -1;
+	size_t count = 0;
+	char first = 'a';
+	char last = 'z';
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (first <= s[i] && s[i] <= last) count++;
+	}
+	return count;
 }

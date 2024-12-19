556194130c32a8a58bdac87e645278a6bb9abd02
Karina Matveeva <matveeva664664@mail.ru>
2024-12-19 10:10:57 +0300

    Update task2.c

--- 3824Б1ФИ2/14 Матвеева Карина Александровна/task2.c
+++ 3824Б1ФИ2/14 Матвеева Карина Александровна/task2.c
@@ -4,5 +4,25 @@
 // Найти длину наименьшей серии.
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+  
+    int n = sizeof(A) / sizeof(A[0]);
+    int min_length = n;
+    int current_length = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (A[i] == A[i - 1]) {
+            current_length++;
+        } else {
+            if (current_length < min_length) {
+                min_length = current_length;
+            }
+            current_length = 1;
+        }
+    }
+
+    if (current_length < min_length) {
+        min_length = current_length;
+    }
+
+   
 }

e9538865cc39ec4b545853ef28122f952e5103d5
Колтаков Александр 3824Б1ПР1 <138163065+rorusi@users.noreply.github.com>
2024-12-19 10:15:26 +0300

    Update task2.c

--- 3824Б1ПР1/11 Колтаков Александр Сергеевич/task2.c
+++ 3824Б1ПР1/11 Колтаков Александр Сергеевич/task2.c
@@ -1,6 +1,25 @@
 // Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
 // Заменить в нем все элементы, встречающиеся более двух раз, на -1.
+int elInArr(int A[], size_t n, int el){
+	int k = 0;
+	for (int i = 0; i < n; i++) {
+		if (A[i] == el)
+			k++;
+	}
+	return k;
+}
+
+void elReplace(int A[], size_t n, int el) {
+	for (int i = 0; i < n; i++) {
+		if (A[i] == el)
+			A[i] = -1;
+	}
+}
+
 void task2(int A[], size_t n)
 {
-  A[0] = 1111111111;
+  for (int i = 0; i < n; i++) {
+	  if (A[i]!=-1)
+		  if (elInArr(A, n, A[i]) > 2) elReplace(A, n, A[i]);
+  }
 }

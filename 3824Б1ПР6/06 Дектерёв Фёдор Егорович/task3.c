77c9ee54b7bedf8e8cd4cb183f61f85b71f81c98
Taino Dergal <fedadekterev3@gmail.com>
2024-12-19 09:36:54 +0300

    Update task3.c

--- 3824Б1ПР6/06 Дектерёв Фёдор Егорович/task3.c
+++ 3824Б1ПР6/06 Дектерёв Фёдор Егорович/task3.c
@@ -1,10 +1,13 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из цифр,
 // содержащихся в исходной строке s.
 // Пример: s = "abc1d2e34f56g", ответ: "123456".
-char* task3(char *s) {
+char* task3(const char *s) {
   
+    if (s == NULL) {
+        return NULL;
+    }
+
     size_t len = strlen(s);
-    
     char *result = (char *)malloc(len + 1);
     if (result == NULL) {
         return NULL;
@@ -12,7 +15,6 @@ char* task3(char *s) {
 
     size_t j = 0;
     for (size_t i = 0; i < len; i++) {
-      
         if (s[i] >= '0' && s[i] <= '9') {
             result[j] = s[i];
             j++;
@@ -20,6 +22,19 @@ char* task3(char *s) {
     }
 
     result[j] = '\0';
-  
-    return result;
+
+    if (j == 0) {
+        free(result);
+        return NULL;
+    }
+
+    char *final_result = (char *)malloc(j + 1);
+    if (final_result == NULL) {
+        free(result);
+        return NULL;
+    }
+    strcpy(final_result, result);
+    free(result);
+
+    return final_result;
 }

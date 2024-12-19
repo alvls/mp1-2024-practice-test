55ccf5cda13cfbc9c27b01251cea538b6de93a19
Luba Uteeva <lubauteeva@gmail.com>
2024-12-19 09:33:21 +0300

    Update task1.c

--- 3824Б1ФИ2/25 Утеева Любовь Владимировна/task1.c
+++ 3824Б1ФИ2/25 Утеева Любовь Владимировна/task1.c
@@ -1,6 +1,20 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего левого соседа.
-size_t task1(int A[], size_t n)
+/*size_t task1(int A[], size_t n)
 {
   return -1;
+}*/
+#include <stdio.h>
+#include <stddef.h>
+size_t task1(int A[], size_t n) {
+    if (n <= 1) {
+        return 0; 
+    }
+    size_t count = 0; 
+    for (int i = 1; i < n; i++) {
+        if (A[i] < A[i - 1]) {
+            count++; 
+        }
+    }
+    return count;
 }

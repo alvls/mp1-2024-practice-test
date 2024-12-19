f2c298fb20a3a9ff433028f49dc65c5a2dcb966b
m <kanukovamargarita@gmail.com>
2024-12-19 09:43:13 +0300

    Update task1.c

--- 3824Б1ПР2/09 Канукова Маргарита Зауровна/task1.c
+++ 3824Б1ПР2/09 Канукова Маргарита Зауровна/task1.c
@@ -2,5 +2,12 @@
 // Найти номер его последнего минимального элемента.
 int task1(int A[], size_t n)
 {
-  return -1;
+  int min = A[0], min_i = 0;
+  for (int i = 0; i < n; i++) {
+    if (A[i] <= min) {
+      min = A[i];
+      min_i = i;
+    }
+  }
+  return min_i;
 }

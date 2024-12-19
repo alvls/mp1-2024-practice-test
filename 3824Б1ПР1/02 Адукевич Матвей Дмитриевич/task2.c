2c0a96f9c6a9f50cb8a2a5e56251b9bf2b376e12
Matvey-Adukevich <matvey.adukevich@gmail.com>
2024-12-19 10:38:20 +0300

    Update task2.c

--- 3824Б1ПР1/02 Адукевич Матвей Дмитриевич/task2.c
+++ 3824Б1ПР1/02 Адукевич Матвей Дмитриевич/task2.c
@@ -3,13 +3,25 @@
 // расстояние |A[k1] – A[k2]| между которыми минимально.
 // Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
 // При невозможности найти пару, вернуть {-1,-1}.
+#include <math.h>
 struct Indices
 {
-  int first_index;
-  int second_index;
+	int first_index;
+	int second_index;
 };
 struct Indices task2(double A[], size_t n)
 {
-  struct Indices indices = {-2, -2};
-  return indices;
+	struct Indices indices = { -1, -1 };
+	int a;
+	double min_razn = fabs(A[1]-A[0]);
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			a = fabs(A[i] - A[j]);
+			if (a < min_razn) {
+				struct Indices indices = { i, j };
+			}
+
+		}
+	}
+	return indices;
 }

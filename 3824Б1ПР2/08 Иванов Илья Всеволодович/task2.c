55ba8e05b1fcffd188f39c9b5a6abdd7ad4c6acb
Oxytochik <oxyrvin@gmail.com>
2024-12-19 10:39:09 +0300

    Update task2.c

--- 3824Б1ПР2/08 Иванов Илья Всеволодович/task2.c
+++ 3824Б1ПР2/08 Иванов Илья Всеволодович/task2.c
@@ -2,7 +2,19 @@
 // Найти значение максимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <limits.h>
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+	int max = -1;
+	for (int x = 1; x <= n - 1;x++) {
+		if (A[x] >= A[x - 1] & A[x] >= A[x + 1]) {
+			if (A[x] > max) {
+				max = A[x];
+			}
+		}
+	}
+	return max;
 }

c7a8022ba70b12d7ecf74dd3fb1c2db1ef3edd81
IntFxZen <86683282+IntFxZen@users.noreply.github.com>
2024-12-19 09:28:44 +0300

    Update task2.c

--- 3824Б1ПР3/03 Блохин Илья Александрович/task2.c
+++ 3824Б1ПР3/03 Блохин Илья Александрович/task2.c
@@ -2,5 +2,20 @@
 // Определить количество разных элементов массива.
 size_t task2(double A[], size_t n)
 {
-  return 0;
+      size_t unique_count = 0;
+
+    for (size_t i = 0; i < n; ++i) {
+        int is_unique = 1;
+        for (size_t j = 0; j < unique_count; ++j) {
+            if (A[i] == A[j]) {
+                is_unique = 0;
+                break;
+            }
+        }
+        if (is_unique) {
+            A[unique_count] = A[i]; 
+            unique_count++; 
+        }
+    }
+    return unique_count;
 }

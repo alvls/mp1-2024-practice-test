ef7ac5fb2979ca6fb55c64704b98ad285cffb0aa
Dmitry Katyshev <181271866+Amour21@users.noreply.github.com>
2024-12-19 09:49:42 +0300

    Update task3.c

--- 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task3.c
+++ 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task3.c
@@ -1,7 +1,26 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, из которой удалены все вхождения символа ch. 
 // Пример: ch = ‘k’, s = “kabckdgkefgk”, ответ: “abcdgefg”.
-char* task3(char *s, char ch)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* task3(char* s, char ch)
 {
-  return "task3";
+    size_t len = strlen(s);
+    char* result = (char*)malloc(len + 1); 
+    if (result == NULL)
+    {
+        return NULL;
+    }
+    size_t j = 0; 
+    for (size_t i = 0; i < len; ++i)
+    {
+        if (s[i] != ch) 
+        {
+            result[j++] = s[i];
+        }
+    }
+    result[j] = '\0';
+    return result; 
 }

8f5a2046be57fddb8e4ff6610ac59bd112fbc9a3
sidleee <sidleee@icloud.com>
2024-12-19 09:45:42 +0300

    Update task3.c

--- 3824Б1ПР1/03 Ахабанин Михаил Сергеевич/task3.c
+++ 3824Б1ПР1/03 Ахабанин Михаил Сергеевич/task3.c
@@ -1,6 +1,15 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
-size_t task3(char *s)
+#include <ctype.h>
+
+size_t task3(char* s)
 {
-  return -1;
+	size_t cnt = 0, i = 0;
+	while (s[i] != '\0') {
+		if (isupper(s[i])) { 
+			cnt++; 
+		}
+		i++;
+	}
+	return cnt;
 }

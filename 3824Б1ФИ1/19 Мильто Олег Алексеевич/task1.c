58e64d737ce15141b706655b39fb64d6bdaf7048
img-gif <Olegmilto15@gmail.com>
2024-12-19 10:15:11 +0300

    Update task1.c

--- 3824Б1ФИ1/19 Мильто Олег Алексеевич/task1.c
+++ 3824Б1ФИ1/19 Мильто Олег Алексеевич/task1.c
@@ -1,7 +1,33 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Преобразовать его, прибавив к четным числам первый элемент,
 // а к нечетным – последний.
-void task1(unsigned A[], size_t N)
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void task1(int A[], size_t N)
 {
-  A[0] = 1111111111;
+    A[0] = 1111111111;
+
+    if (N > 0)
+    {
+        int firstElement = A[0];
+        int lastElement = A[N - 1];
+
+        for (int i = 0; i < N; ++i)
+        {
+            if (A[i] % 2 == 0)
+            {
+                A[i] += firstElement;
+            }
+            else
+            {
+                A[i] += lastElement;
+            }
+        }
+    }
+    for (int i = 0; i < N; ++i)
+    {
+        printf("%d ", A[i]);
+    }
 }

04a9eb81dd8296fc8d527c50016817b4b22efd6e
Екатерина Серова <katerinaserova505@gmail.com>
2024-12-19 09:30:58 +0300

    Update task1.c

--- 3824Б1ПР1/25 Серова Екатерина Борисовна/task1.c
+++ 3824Б1ПР1/25 Серова Екатерина Борисовна/task1.c
@@ -1,6 +1,14 @@
+
+#include <stdio.h>
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего левого соседа.
 size_t task1(int A[], size_t n)
 {
-  return -1;
+	size_t cnt = 0;
+	for (size_t i = 1; i < n; i++) {
+		if (A[i] < A[i - 1]) {
+			cnt++;
+		}
+	}
+	return cnt;
 }

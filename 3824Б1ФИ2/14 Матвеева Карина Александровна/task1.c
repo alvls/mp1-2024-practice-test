f77304f28f623fcdfec829442d5e38691842f415
Karina Matveeva <matveeva664664@mail.ru>
2024-12-19 10:13:48 +0300

    Update task1.c

--- 3824Б1ФИ2/14 Матвеева Карина Александровна/task1.c
+++ 3824Б1ФИ2/14 Матвеева Карина Александровна/task1.c
@@ -1,7 +1,24 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Найти номер последнего экстремального (минимального или максимального) 
 // элемента в нем.
+#include <stdio.h>
+#include <stdlib.h>
+
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+  int i, n, ni, min, max, minn, maxn;
+printf("N:");
+scanf_s("%i", &n);
+for (i = 1; i <= n; ++i) {
+    printf("%i>", i);
+    scanf_s("%i", &ni);
+    if ((i == 1) || (ni < min)) {
+        min = ni; minn = i;
+    }
+    if ((i == 1) || (ni > max)) {
+        max = ni; maxn = i;
+    }
+}
+
+return 0;
 }

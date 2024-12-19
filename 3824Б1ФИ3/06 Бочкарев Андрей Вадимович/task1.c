b312a0d9727f5bd545bc2aa0c67f19e41b62c765
AndreiStendaper <andreipirozhochek@gmail.com>
2024-12-19 09:43:49 +0300

    Update task1.c

--- 3824Б1ФИ3/06 Бочкарев Андрей Вадимович/task1.c
+++ 3824Б1ФИ3/06 Бочкарев Андрей Вадимович/task1.c
@@ -2,8 +2,8 @@
 // Найти сколько чисел в массиве меньше своего правого соседа.
 size_t task1(int A[], size_t n)
 {
-  int c = 0;
-  for (int i=0;i<n-1;i++)
+  size_t c = 0;
+  for (size_t i=0;i<n-1;i++)
     {
       if (A[i]<A[i+1]) c++;
     }

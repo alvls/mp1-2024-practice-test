e5d524a5c8914668c0451813d7476ac5af981664
Антон Кондрашкин <821019@bk.ru>
2024-12-19 10:33:21 +0300

    Update task3.c

--- 3824Б1ФИ1/16 Кондрашкин Антон Алексеевич/task3.c
+++ 3824Б1ФИ1/16 Кондрашкин Антон Алексеевич/task3.c
@@ -1,6 +1,19 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить количество слов в строке.
-int task3(char *s)
-{
-  return -1;
+int task3(char* s) {
+    int word_count = 0;
+    int in_word = 0;
+    while (*s != '\0') {
+        if (*s != ' ') {
+            if (in_word == 0) {
+                word_count++;
+                in_word = 1; 
+            }
+        }
+        else {
+            in_word = 0;
+        }
+        s++;
+    }
+    return word_count; 
 }

4dcaa0efc92c0a67da22e4eac36bf26d8c8ea460
Юлия <uzabytina9@gmail.com>
2024-12-19 10:28:17 +0300

    Update task2.c

--- 3824Б1ПР2/07 Забытина Юлия Артемовна/task2.c
+++ 3824Б1ПР2/07 Забытина Юлия Артемовна/task2.c
@@ -2,7 +2,27 @@
 // Найти значение минимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <limits.h>
+long long task2(unsigned A[], size_t n){
+	int local_mins_c = 0;
+
+	long long min_of_all_mins = LLONG_MAX;
+	int local_min = 0;
+
+	for (int i = 1; i < n-1; i++) {
+		if ( (A[i] <= A[i - 1]) && (A[i] <= A[i + 1]) ) {
+			local_min = A[i];
+			local_mins_c ++;
+		}
+		if (local_min <= min_of_all_mins) {
+			min_of_all_mins = local_min;
+		}
+	}
+
+	if (local_mins_c == 0) {
+		return -1;
+	}
+	else {
+		return min_of_all_mins;
+	}
 }

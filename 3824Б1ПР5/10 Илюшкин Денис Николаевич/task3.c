7ed986b984fd0f27556e2359a7f517e4efefffb9
Lopeshss <diluskin04@gmail.com>
2024-12-19 09:38:43 +0300

    Update task3.c

--- 3824Б1ПР5/10 Илюшкин Денис Николаевич/task3.c
+++ 3824Б1ПР5/10 Илюшкин Денис Николаевич/task3.c
@@ -4,7 +4,28 @@
 //   то включить в ss N первых символов строки s.
 // Если длина строки s меньше N,
 //   то включить в ss все символы строки s, добавив после них символы '&'.
+#include <string.h>
+#include <stdlib.h>
+
 char* task3(char *s, unsigned N)
 {
-  return "task3";
+    unsigned len = strlen(s);
+
+    char *ss = (char *)malloc((N + 1) * sizeof(char));
+    if (!ss) {
+        return NULL;
+    }
+
+    if (len >= N) {
+        memcpy(ss, s, N);
+        ss[N] = '\0';
+    } else {
+        memcpy(ss, s, len);
+        for (unsigned i = len; i < N; i++) {
+            ss[i] = '&';
+        }
+        ss[N] = '\0';
+    }
+
+    return ss;
 }

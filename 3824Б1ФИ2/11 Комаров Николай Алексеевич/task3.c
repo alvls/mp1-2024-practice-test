01b57b34751e847f4c62e301fd8a622155e26c67
NikolayKomarovv <komarovl1974@gmail.com>
2024-12-19 10:09:31 +0300

    Update task3.c

--- 3824Б1ФИ2/11 Комаров Николай Алексеевич/task3.c
+++ 3824Б1ФИ2/11 Комаров Николай Алексеевич/task3.c
@@ -6,7 +6,28 @@
 // Пример: s1 = "aaa", N1 = 4, s2 = "bbb", N2 = 4
 //   Ответ: "aaabbb"
 // Функции стандартной библиотеки языка С не применять.
-char* task3(char *s1, unsigned N1, char *s2, unsigned N2)
-{
-  return "task3";
+#include <stdio.h>
+#include <memory.h>
+char* task3(char *s1, unsigned N1, char *s2, unsigned N2) {
+    unsigned len1 = 0, len2 = 0;
+    while (s1[len1] != '\0') {
+        len1++;
+    }
+    while (s2[len2] != '\0') {
+        len2++;
+    }
+    unsigned take_from_s1 = N1 < len1 ? N1 : len1;
+    unsigned take_from_s2 = N2 < len2 ? N2 : len2;
+    char *result = (char*) malloc((take_from_s1 + take_from_s2 + 1) * sizeof(char));  
+    if (result == NULL) {
+        return NULL;
+    }
+    for (unsigned i = 0; i < take_from_s1; i++) {
+        result[i] = s1[i];
+    }
+    for (unsigned i = 0; i < take_from_s2; i++) {
+        result[take_from_s1 + i] = s2[len2 - take_from_s2 + i];
+    }
+    result[take_from_s1 + take_from_s2] = '\0';
+    return result;
 }

afd81b489832f7b87fe0e7a3b7935ec70a91f1f4
NeFade <108527055+NeFade@users.noreply.github.com>
2024-12-19 10:26:13 +0300

    Update task3.c

--- 3824Б1ПР6/01 Абрахин Артем Дмитриевич/task3.c
+++ 3824Б1ПР6/01 Абрахин Артем Дмитриевич/task3.c
@@ -1,6 +1,19 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
-char* task3(char *s)
-{
-  return "task3";
+char* task3(char* s) {
+    int len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    static char reversed[100];
+    int i;
+
+    for (i = 0; i < len; ++i) {
+        reversed[i] = s[len - i - 1];
+    }
+
+    reversed[len] = '\0';
+
+    return reversed;
 }

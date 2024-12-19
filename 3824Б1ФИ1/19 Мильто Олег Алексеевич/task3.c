0277d1da20306dbd0a8004ff20b0069b8a336cdf
img-gif <Olegmilto15@gmail.com>
2024-12-19 10:17:19 +0300

    Update task3.c

--- 3824Б1ФИ1/19 Мильто Олег Алексеевич/task3.c
+++ 3824Б1ФИ1/19 Мильто Олег Алексеевич/task3.c
@@ -1,7 +1,43 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Скопировать строку s в новую и преобразовать ее так,
 // чтобы все слова в ней начинались с заглавной буквы.
-char* task3(char *s)
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *task3(char *s)
 {
-  return "task3";
+
+  char *result = malloc(strlen(s) + 1);
+
+  if (!result)
+  {
+    return NULL;
+  }
+
+  char *src = s;
+  char *dest = result;
+
+  while (*src != '\0')
+  {
+
+    *dest++ = toupper((unsigned char)*src++);
+
+    while (*src && !isspace((unsigned char)*src))
+    {
+      *dest++ = *src++;
+    }
+
+    while (*src && isspace((unsigned char)*src))
+    {
+      *dest++ = ' ';
+      src++;
+    }
+  }
+
+  *dest = '\0';
+
+  return result;
+  free(result);
 }

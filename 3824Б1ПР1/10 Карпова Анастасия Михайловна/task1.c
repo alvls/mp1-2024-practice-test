a984be202368920f2bab4efde958f85dda1ee062
KarpovaAnastasia <karpovaanastasia4417@gmail.com>
2024-12-19 09:28:14 +0300

    Update task1.c

--- 3824Б1ПР1/10 Карпова Анастасия Михайловна/task1.c
+++ 3824Б1ПР1/10 Карпова Анастасия Михайловна/task1.c
@@ -1,6 +1,19 @@
 // Задача 1. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти номер его последнего максимального элемента.
+#include <limits.h>
 int task1(int A[], size_t n)
 {
-  return -1;
+    if (n == 0) {
+        return -1;
+
+    int max_element = INT_MIN;
+    int max_index = -1;
+
+    for (size_t i = 0; i < n; ++i) {
+        if (A[i] >= max_element) {
+            max_element = A[i];
+            max_index = i;
+        }
+    }
+    return max_index;
 }

057c78abc60e62c18e865f01b3dc1ebb8d8870cc
qwertewer <91060905+qwertewer@users.noreply.github.com>
2024-12-19 10:23:31 +0300

    Update task3.c

--- 3824Б1ФИ2/01 Авдеев Денис Олегович/task3.c
+++ 3824Б1ФИ2/01 Авдеев Денис Олегович/task3.c
@@ -1,6 +1,14 @@
 // Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
 // но расположенные в обратном порядке.
-char* task3(char *s)
-{
-  return "task3";
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char* task3(char* s) {
+    size_t n = strlen(s); 
+    char* rev = (char*)malloc(n + 1);
+    for (size_t i = 0; i < n; ++i) {
+        rev[i] = s[n - 1 - i];
+    }
+    rev[n] = '\0';
+    return rev;
 }

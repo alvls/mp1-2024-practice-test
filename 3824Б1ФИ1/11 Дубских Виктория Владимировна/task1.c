68bf9ec1b36e396b0d23311c54b6555c9bfdd990
vicus-ux <viktoriadubskih@gmail.com>
2024-12-19 10:01:22 +0300

    Update task1.c

--- 3824Б1ФИ1/11 Дубских Виктория Владимировна/task1.c
+++ 3824Б1ФИ1/11 Дубских Виктория Владимировна/task1.c
@@ -1,6 +1,9 @@
 // Задача 1. Даны числа a, b (0 < a < b) и непустой массив A из N элементов.
 // Найти минимальный из тех элементов A, которые содержатся в интервале (a, b).
 // Вернуть 0, если требуемые элементы отсутствуют.
+#include <stdio.h>
+#include <limits.h>
+
 int task1(unsigned A[], size_t n, unsigned a, unsigned b) {
     unsigned min = UINT_MAX; //min value make max for unsigned
 

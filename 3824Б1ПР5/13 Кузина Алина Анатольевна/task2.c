8ff91ff9b85067cbbb68f62bfc0e667e0d19c934
KuzinaAlina <aragoto2005@gmail.com>
2024-12-19 09:15:06 +0300

    Update task2.c

--- 3824Б1ПР5/13 Кузина Алина Анатольевна/task2.c
+++ 3824Б1ПР5/13 Кузина Алина Анатольевна/task2.c
@@ -2,7 +2,30 @@
 // Назовем «серией» группу подряд идущих одинаковых элементов, 
 // а длиной серии – количество этих элементов (длина серии может быть равна 1).
 // Найти длину наибольшей серии.
-int task2(unsigned A[], size_t n)
-{
-  return -1;
+#include <stddef.h>
+
+int task2(unsigned A[], size_t n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    unsigned max_length = 1; 
+    unsigned current_length = 1;  
+
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] == A[i - 1]) {
+            current_length++; 
+        } else {
+            if (current_length > max_length) {
+                max_length = current_length; 
+            }
+            current_length = 1; 
+        }
+    }
+
+    if (current_length > max_length) {
+        max_length = current_length;
+    }
+
+    return max_length; 
 }

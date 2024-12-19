dbaac2db2e588082888eddc133680653a5009004
loesq <116908536+loesq@users.noreply.github.com>
2024-12-19 10:08:18 +0300

    Update task2.c

--- 3824Б1ПР4/27 Ширяев Иван Александрович/task2.c
+++ 3824Б1ПР4/27 Ширяев Иван Александрович/task2.c
@@ -2,7 +2,19 @@
 // Найти значение минимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+long long task2(unsigned A[], size_t n) 
+{ 
+    if (n < 3) return -1; 
+ 
+    long long min_local = -1; 
+ 
+    for (size_t i = 1; i < n - 1; ++i) 
+    { 
+        if (A[i] <= A[i - 1] && A[i] <= A[i + 1])   
+        { 
+            if (min_local == -1 || A[i] < min_local) 
+                min_local = A[i]; 
+        } 
+    } 
+    return min_local; 
 }

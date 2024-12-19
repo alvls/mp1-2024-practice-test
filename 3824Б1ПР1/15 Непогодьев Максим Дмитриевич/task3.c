822c92c579bc12ca19cf6898b918c5ced11f1ba7
ionfy <maksnepogodev@gmail.com>
2024-12-19 09:46:46 +0300

    Update task3.c

--- 3824Б1ПР1/15 Непогодьев Максим Дмитриевич/task3.c
+++ 3824Б1ПР1/15 Непогодьев Максим Дмитриевич/task3.c
@@ -1,7 +1,21 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, из которой удалены все вхождения символа ch. 
 // Пример: ch = ‘k’, s = “kabckdgkefgk”, ответ: “abcdgefg”.
+#include <stdlib.h>
+
 char* task3(char *s, char ch)
 {
-  return "task3";
+  char* ans = nullptr;
+  int n = 0, k = 0;
+  while (s[n] != '\0') {
+  	n++;
+  }
+  ans = (char*)malloc((n + 1) * sizeof(char));
+  for (int i = 0; i <= n; i++) {
+  	if (s[i] != ch) {
+  		ans[k] = s[i];
+  		k++;
+  	}
+  }
+  return ans;
 }

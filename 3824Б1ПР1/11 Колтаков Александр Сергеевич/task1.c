bdb4f16fe8132e269da92495f0c09ac189928e55
Колтаков Александр 3824Б1ПР1 <138163065+rorusi@users.noreply.github.com>
2024-12-19 09:42:42 +0300

    Update task1.c

--- 3824Б1ПР1/11 Колтаков Александр Сергеевич/task1.c
+++ 3824Б1ПР1/11 Колтаков Александр Сергеевич/task1.c
@@ -3,5 +3,11 @@
 // Вернуть 0, если требуемые элементы отсутствуют.
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+  int min_e = b;
+  for (int i = 0; i < n; i++) {
+	  if ((A[i] > a) && (A[i] < b))
+		  if (A[i] < min_e) min_e = A[i];
+  }
+  if (min_e = b) return 0;
+  else return min_e;
 }

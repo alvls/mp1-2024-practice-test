f3ecd9344b7799a0941adbd270729654ba5dbdc8
Зверев Никита Олегович <mrnikitos098@gmail.com>
2024-12-19 10:32:56 +0300

    Update task3.c

--- 3824Б1ПР5/08 Зверев Никита Олегович/task3.c
+++ 3824Б1ПР5/08 Зверев Никита Олегович/task3.c
@@ -3,5 +3,16 @@
 // Пример: s = "aBc1D2e34F56g", ответ: "aceg".
 char* task3(char *s)
 {
-  return "task3";
+      size_t len = strlen(s);
+    char *result = malloc(len + 1);
+    if (!result) return NULL;
+
+    size_t j = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (islower(s[i])) {
+            result[j++] = s[i];
+        }
+    }
+    result[j] = '\0';
+  return result;
 }

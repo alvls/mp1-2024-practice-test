423a3b0adc5b92a422bae542b4fdcc4990071af0
ProkofievEgor <fenckmmk@gmail.com>
2024-12-19 10:03:40 +0300

    Update task3.c

--- 3824Б1ФИ2/19 Прокофьев Егор Олегович/task3.c
+++ 3824Б1ФИ2/19 Прокофьев Егор Олегович/task3.c
@@ -1,7 +1,19 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Скопировать строку s в новую и преобразовать ее так,
 // чтобы все слова в ней начинались с заглавной буквы.
-char* task3(char *s)
-{
-  return "task3";
+char* task3(char *s) {
+    char *result = (char*) malloc(strlen(s) + 1);
+    strcpy(result, s);
+
+    int inWord = 0;
+    for (size_t i = 0; result[i]; i++) {
+        if (isspace(result[i])) {
+            inWord = 0;
+        } else if (!inWord) {
+            inWord = 1;
+            result[i] = toupper(result[i]);
+        }
+    }
+
+    return result;
 }

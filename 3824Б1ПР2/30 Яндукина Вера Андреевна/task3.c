69095d635df086e31514947c86928df80f01d490
Vera <verayandukina@yandex.ru>
2024-12-19 10:34:15 +0300

    Update task3.c

--- 3824Б1ПР2/30 Яндукина Вера Андреевна/task3.c
+++ 3824Б1ПР2/30 Яндукина Вера Андреевна/task3.c
@@ -4,7 +4,26 @@
 //   то включить в ss N первых символов строки s.
 // Если длина строки s меньше N,
 //   то включить в ss все символы строки s, добавив после них символы '&'.
+#include <stdio.h>
+#include <stdlib.h>
+
 char* task3(char *s, unsigned N)
 {
-  return "task3";
+  size_t len = strlen(s);
+  char* ss = (char*)malloc((N+1) * sizeof(char));
+  if (len >= N) {
+    for (size_t i = 0; i <= N; i++) {
+        ss[i] = s[i];
+    }
+   }
+  else {
+    for (size_t i = 0; i <= len; i++) {
+        ss[i] = s[i];
+    }
+    for (size_t i = len; i < N; i++) {
+        ss[i] = '&';
+    }
+   }
+  ss[N] = '\0';
+  return ss;
 }

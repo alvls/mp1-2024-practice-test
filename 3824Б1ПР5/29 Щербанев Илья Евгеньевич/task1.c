fa0a6daaa0a0cb4c4c06fe6c89ec1661feb777a9
Pr13rak9 <shevegasa@gmail.com>
2024-12-19 10:03:44 +0300

    Update task1.c

--- 3824Б1ПР5/29 Щербанев Илья Евгеньевич/task1.c
+++ 3824Б1ПР5/29 Щербанев Илья Евгеньевич/task1.c
@@ -1,22 +1,22 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
-#include <algorithm>
+#include <stddef.h>
 
 int task1(int A[], size_t n)
 {
     if (n == 0) {
         return -1; // Массив пустой
     }
-    
-    int min_value = A[0];
-    int min_index = 0;
-    
+
+    int minIndex = 0;
+    int minValue = A[0];
+
     for (size_t i = 1; i < n; ++i) {
-        if (A[i] <= min_value) { // Если нашли новый минимум или равный ему
-            min_value = A[i];    // Обновляем значение минимума
-            min_index = i;       // Запоминаем индекс нового минимума
+        if (A[i] < minValue) {
+            minValue = A[i];
+            minIndex = i;
         }
     }
-    
-    return min_index;
+
+    return minIndex;
 }

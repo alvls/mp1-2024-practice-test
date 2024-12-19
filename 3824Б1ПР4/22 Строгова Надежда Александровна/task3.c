3052658f1d453ee150e53ab930aecdc5bd73daf0
StrogovaNadezhda <Strogova02@inbox.ru>
2024-12-19 10:24:51 +0300

    Update task3.c

--- 3824Б1ПР4/22 Строгова Надежда Александровна/task3.c
+++ 3824Б1ПР4/22 Строгова Надежда Александровна/task3.c
@@ -1,5 +1,13 @@
 // Задача 3. Дана строка s. Подсчитать количество содержащихся в ней цифр.
-size_t task3(char *s)
-{
-  return -1;
+#include <stdio.h>
+#include <ctype.h>
+
+size_t task3(char *s) {
+    size_t count = 0;
+    for (; *s != '\0'; s++) {
+        if (isdigit(*s)) {
+            count++;
+        }
+    }
+    return count;
 }

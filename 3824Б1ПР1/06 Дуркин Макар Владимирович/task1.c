3a6b14f1af1a32e516d0a3fea3589589b7f01898
MakarXtreme2 <durkinv21@gmail.com>
2024-12-19 09:22:03 +0300

    Update task1.c
    Task1

--- 3824Б1ПР1/06 Дуркин Макар Владимирович/task1.c
+++ 3824Б1ПР1/06 Дуркин Макар Владимирович/task1.c
@@ -2,5 +2,13 @@
 // Найти сколько чисел в массиве меньше своего правого соседа.
 size_t task1(int A[], size_t n)
 {
-  return -1;
+  size_t mx = 0;
+  if (n == 1)
+  	return 0;
+  for (int i = 0; i < n - 1; i++)
+  {
+  	if (A[i] < A[i + 1])
+  		mx++;
+  }
+  return mx;
 }

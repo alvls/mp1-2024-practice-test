67a1b9db83df7f93db473f90bc927c3f1b36c822
FivelC <121977776+FivelC@users.noreply.github.com>
2024-12-19 10:25:54 +0300

    Update task1.c

--- 3824Б1ПР2/13 Лапин Егор Алексеевич/task1.c
+++ 3824Б1ПР2/13 Лапин Егор Алексеевич/task1.c
@@ -3,5 +3,19 @@
 // элемента в нем.
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+  unsigned min = A[0];
+  unsigned number_element_min = 0;
+  unsigned max = A[0];
+  unsigned number_element_max = 0;
+  for (unsigned i = 0; i < N; i++) {
+      if (A[i] < min) { min = A[i]; number_element_min = i; }
+      if (A[i] > max) { max = A[i]; number_element_max = i; }
+  }
+  if (number_element_min < number_element_max) {
+      return number_element_min;
+  }
+  else if (number_element_min > number_element_max) {
+      return number_element_max;
+  }
+  return 0;
 }

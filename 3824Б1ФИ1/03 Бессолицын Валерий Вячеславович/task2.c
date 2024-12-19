012e7dd18210cb9117a4c7ccb938cb636f64ff0b
Valerian <139715129+Molingen@users.noreply.github.com>
2024-12-19 09:24:43 +0300

    Update task2.c

--- 3824Б1ФИ1/03 Бессолицын Валерий Вячеславович/task2.c
+++ 3824Б1ФИ1/03 Бессолицын Валерий Вячеславович/task2.c
@@ -1,6 +1,25 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
-size_t task2(double A[], size_t n)
-{
-  return 0;
+#include <stdlib.h>
+
+int compare(const void * a, const void * b) {
+    const double diff = *(double*)a - *(double*)b;
+    if (diff > 0.0) return 1;
+    if (diff < 0.0) return -1;
+    return 0;
+}
+
+size_t task2(double * A, size_t n) {
+    if (n == 0) return 0;
+
+    qsort(A, n, sizeof(double), compare);
+
+    size_t unique_count = 1;
+    for (size_t i = 1; i < n; ++i) {
+        if (A[i] != A[i - 1]) {
+            ++unique_count;
+        }
+    }
+
+    return unique_count;
 }

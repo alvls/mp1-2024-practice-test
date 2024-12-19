deb331b8f1ab76529807cfe94ea94b3cf2713914
alex00turbo <alex2006turbo@gmail.com>
2024-12-19 10:38:02 +0300

    Arisov Alexandr_3.c

--- 3824Б1ПР5/01 Арисов Александр Сергеевич/task3.c
+++ 3824Б1ПР5/01 Арисов Александр Сергеевич/task3.c
@@ -1,6 +1,21 @@
-// Задача 3. Дана строка s. Сформировать новую строку, содержащую те же символы,
-// но расположенные в обратном порядке.
-char* task3(char *s)
-{
-  return "task3";
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char *s) {
+  size_t len = strlen(s);  
+  char *reversed = (char*)malloc(len + 1);  
+
+  for (size_t i = 0; i < len; i++) {
+    reversed[i] = s[len - 1 - i];
+  }
+
+  reversed[len] = '\0'; 
+
+  return reversed;  
+}
+
+void _start() {
+  char s[] = "hello";  
+  char *reversed = task3(s);  
+  free(reversed);  
 }

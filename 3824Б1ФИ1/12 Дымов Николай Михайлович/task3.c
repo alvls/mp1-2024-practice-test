3e543b9adfb78d4b6317ef15e1e7b3a319701936
thevosim <thegalaxyc@gmail.com>
2024-12-19 10:27:07 +0300

    Update task3.c

--- 3824Б1ФИ1/12 Дымов Николай Михайлович/task3.c
+++ 3824Б1ФИ1/12 Дымов Николай Михайлович/task3.c
@@ -1,25 +1,25 @@
 #include <stdlib.h>
 char* task3(char* s, char ch) 
 {
-  int count = 0;
+	int count = 0;
 	int len = 0;
-  for (char* ptr = s; *ptr; ++ptr) 
-  {
-  ++len;
-      if (*ptr == ch)
-          ++count;
-  }
+	for (char* ptr = s; *ptr; ++ptr) 
+	{
+	++len;
+	if (*ptr == ch)
+	  ++count;
+	}
 
-  int n = len + count + 1; 
-  char* result = (char*)malloc(n);
-
-  char* dest = result;
-  for (char *ptr = s; *ptr; ++ptr) 
-  {
-    *dest++ = *ptr;
-    if (*ptr == ch)
-        *dest++ = ch;
-  }
-  *dest = '\0';
-  return result;
+	int n = len + count + 1; 
+	char* result = (char*)malloc(n);
+	
+	char* dest = result;
+	for (char *ptr = s; *ptr; ++ptr) 
+	{
+	*dest++ = *ptr;
+	if (*ptr == ch)
+		*dest++ = ch;
+	}
+	*dest = '\0';
+	return result;
 }

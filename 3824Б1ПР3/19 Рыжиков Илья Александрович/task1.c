1cc6153fa3fbd833c6c74da513a42c4b53a45484
ryzhil <119815908+ryzhil@users.noreply.github.com>
2024-12-19 09:12:37 +0300

    Update task1.c

--- 3824Б1ПР3/19 Рыжиков Илья Александрович/task1.c
+++ 3824Б1ПР3/19 Рыжиков Илья Александрович/task1.c
@@ -1,7 +1,20 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Преобразовать его, прибавив к четным числам первый элемент,
 // а к нечетным – последний.
+#include <stdio.h>
+
 void task1(unsigned A[], size_t N)
 {
-  A[0] = 1111111111;
+    if (N == 0) return;
+
+    unsigned first = A[0];
+    unsigned last = A[N-1];
+
+    for (size_t i = 0; i < N; i++) {
+        if (A[i] % 2 == 0) {
+            A[i] += first;
+        } else {
+            A[i] += last;
+        }
+    }
 }

78d0e7f621a8769a849376dab68ec3d565dd9607
Anastasia <76954665+Belphegor06@users.noreply.github.com>
2024-12-19 10:19:22 +0300

    Update task3.c

--- 3824Б1ПР2/04 Дементьева Анастасия Владимировна/task3.c
+++ 3824Б1ПР2/04 Дементьева Анастасия Владимировна/task3.c
@@ -1,6 +1,16 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней строчных букв английского алфавита.
-size_t task3(char *s)
+#include <stddef.h>
+
+size_t task3(char* s)
 {
-  return -1;
+    size_t count = 0;
+
+    for (char* p = s; *p != '\0'; p++) {
+        if (97 <= *p && *p <= 122) {
+            count++;
+        }
+    }
+
+    return count;
 }

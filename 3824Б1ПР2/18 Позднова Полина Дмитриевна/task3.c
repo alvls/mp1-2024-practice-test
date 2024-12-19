c38e91620344eeb1869ce9ac41ed358ee5fc51c2
Polina0013 <123825611+Polina0013@users.noreply.github.com>
2024-12-19 10:22:12 +0300

    Update task3.c

--- 3824Б1ПР2/18 Позднова Полина Дмитриевна/task3.c
+++ 3824Б1ПР2/18 Позднова Полина Дмитриевна/task3.c
@@ -1,6 +1,26 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить длину самого короткого слова в строке.
+
+
 int task3(char *s)
 {
-  return -1;
+  if (s == NULL) return -1;
+  int min_len = -1; 
+  int current_len = 0; 
+  while (*s) {
+    while (*s == ' ') {
+      s++;
+    }
+    current_len = 0;
+    while (*s != ' ' && *s != '\0') {
+      current_len++;
+      s++;
+    }
+    if (current_len > 0) {
+      if (min_len == -1 || current_len < min_len) {
+        min_len = current_len;
+      }
+    }
+  }
+  return min_len; 
 }

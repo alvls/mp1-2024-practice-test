d126244399a22f82a8ae9300466eba7e53dfa75f
LosTikkk <77080876+LosTikkk@users.noreply.github.com>
2024-12-19 10:28:25 +0300

    Update task2.c

--- 3824Б1ПР5/14 Лапшов Андрей Андреевич/task2.c
+++ 3824Б1ПР5/14 Лапшов Андрей Андреевич/task2.c
@@ -1,8 +1,30 @@
-// Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
-// Назовем «серией» группу подряд идущих одинаковых элементов, 
-// а длиной серии – количество этих элементов (длина серии может быть равна 1).
-// Найти длину наименьшей серии.
+#include <stddef.h>
+
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+    if (n == 0) {
+        return -1; // массив пуст
+    }
+
+    size_t min_length = 1;
+    size_t current_length = 1;
+    unsigned prev = A[0];
+
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] == prev) {
+            current_length++;
+        } else {
+            if (current_length < min_length || min_length == -1) {
+                min_length = current_length;
+            }
+            current_length = 1;
+        }
+        prev = A[i];
+    }
+    
+    if (current_length < min_length || min_length == -1) {
+        min_length = current_length;
+    }
+
+    return min_length;
 }

5dc23d9c021a1e0a0fdb7036cf85a81242d51976
Yournoy <taras7777feduloff@gmail.com>
2024-12-19 10:21:08 +0300

    Update task3.c

--- 3824Б1ПР4/23 Федулов Тарас Денисович/task3.c
+++ 3824Б1ПР4/23 Федулов Тарас Денисович/task3.c
@@ -1,6 +1,16 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
-size_t task3(char *s)
+#include <stdio.h>
+
+size_t task3(char *s) 
 {
-  return -1;
+    size_t count = 0;
+    for (int i = 0; s[i] != '\0'; i++) 
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z') 
+        {
+            count++;
+        }
+    }
+    return count;
 }

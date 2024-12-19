2a244eac59fbd5629cf06715440ed12b05f88e8d
Micro4elikkk <95868942+Micro4elikkk@users.noreply.github.com>
2024-12-19 09:45:37 +0300

    Update task3.c

--- 3824Б1ПР1/23 Русановский Никита Владимирович/task3.c
+++ 3824Б1ПР1/23 Русановский Никита Владимирович/task3.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней заглавных букв английского алфавита.
 size_t task3(char *s)
 {
-  return -1;
+	size_t cntr = 0;
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (isupper(s[i])) {
+			cntr++;
+		}
+	}
+	return cntr;
 }

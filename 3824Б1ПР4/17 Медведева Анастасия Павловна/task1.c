69a54a752940687d7d91a0b69f0ae1e4ea867478
anastaciaaaa13 <158077069+anastaciaaaa13@users.noreply.github.com>
2024-12-19 09:21:45 +0300

    Update task1.c

--- 3824Б1ПР4/17 Медведева Анастасия Павловна/task1.c
+++ 3824Б1ПР4/17 Медведева Анастасия Павловна/task1.c
@@ -1,6 +1,14 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Переставить его элементы в обратном порядке.
-void task1(int A[], size_t N)
-{
-  A[0] = 1111111111;
+#include <stdio.h>
+
+void task1(int A[], size_t N) {
+
+    for (size_t i = 0; i < N / 2; i++) {
+        int temp = A[i];
+        A[i] = A[N - 1 - i];
+        A[N - 1 - i] = temp;
+    }
+    
+    A[0] = 1111111111;
 }

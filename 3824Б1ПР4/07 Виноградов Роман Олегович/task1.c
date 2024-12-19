c940cf51f330e876c347fc299e2e01e7a2a70f4c
Виноградов Роман <romanvinogradov476@gmail.com>
2024-12-19 09:53:23 +0300

    Update task1.c

--- 3824Б1ПР4/07 Виноградов Роман Олегович/task1.c
+++ 3824Б1ПР4/07 Виноградов Роман Олегович/task1.c
@@ -1,7 +1,32 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между первыми
 // двумя нулями.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <stddef.h>
+
+int task1(int A[], size_t n) {
+    size_t first_zero = -1;
+    size_t second_zero = -1;
+    int sum = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        if (A[i] == 0) {
+            if (first_zero == -1) {
+                first_zero = i;
+            } else {
+                second_zero = i;
+                break;
+            }
+        }
+    }
+
+    if (first_zero == -1 || second_zero == -1 || first_zero == second_zero - 1) {
+        return -1;
+    }
+
+    for (size_t i = first_zero + 1; i < second_zero; i++) {
+        sum += A[i];
+    }
+
+    return sum;
 }
+

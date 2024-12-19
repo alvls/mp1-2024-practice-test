6add9de252331d909267a3a7f83065ada3aabebe
Ksenia <109479530+Ksenia555@users.noreply.github.com>
2024-12-19 09:39:06 +0300

    Update task1.c

--- 3824Б1ФИ1/28 Федорова Ксения Димитриевна/task1.c
+++ 3824Б1ФИ1/28 Федорова Ксения Димитриевна/task1.c
@@ -3,5 +3,27 @@
 // двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1111111111;
+	int sum = 0, count = 0;
+	int ind_1 = 0, ind_2 = 0;
+	for (int i = 0; i < n; i++)
+	{
+		count += (A[i] == 0) ? 1 : 0;
+	}
+
+	count++;
+	for (int i = 0; i < n; i++)
+	{
+		if (A[i] == 0) count--;
+		
+		if (A[i] == 0 && count == 2) ind_1 = i;
+
+		if (A[i] == 0 && count == 1) ind_2 = i;
+	}
+	
+	for (ind_1; ind_1 < ind_2; ind_1++)
+	{
+		sum += A[ind_1];
+	}
+
+	return sum;
 }

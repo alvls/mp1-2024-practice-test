6620a007e131c39d5d21fe5ae75c009e5d088381
trippalm <125157805+trippalm@users.noreply.github.com>
2024-12-19 09:52:15 +0300

    Update task3.c

--- 3824Б1ФИ3/13 Иудин Арсений Александрович/task3.c
+++ 3824Б1ФИ3/13 Иудин Арсений Александрович/task3.c
@@ -2,7 +2,33 @@
 // Сформировать новую строку, в которой после каждого вхождения символа ch 
 // в строку s вставлен символ ‘#’.
 // Пример: ch = ‘k’, s = “abckdgkefgk”, ответ: “abck#dgk#efgk#”.
-char* task3(char *s, char ch)
-{
-  return "task3";
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char* task3(char *s, char ch) {
+    // Подсчитаем количество вхождений ch в строку s
+    size_t count = 0;
+    for (size_t i = 0; s[i] != '0'; i++) {
+        if (s[i] == ch) {
+            count++;
+        }
+    }
+    // Выделяем память для новой строки
+    size_t new_length = strlen(s) + count + 1; // +1 для нулевого терминатора
+    char *new_str = (char *)malloc(new_length * sizeof(char));
+    if (new_str == NULL) {
+        return NULL; // Проверка на успешное выделение памяти
+    }
+
+    size_t j = 0; // Индекс для новой строки
+    for (size_t i = 0; s[i] != '0'; i++) {
+        new_str[j++] = s[i]; // Копируем текущий символ
+        if (s[i] == ch) {
+            new_str[j++] = '#'; // Вставляем '#' после ch
+        }
+    }
+    
+    new_str[j] = '0'; // Завершаем новую строку нулевым символом
+
+    return new_str; // Возвращаем новую строку
 }

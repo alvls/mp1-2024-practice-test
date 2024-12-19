dd918cea438d0657381b2ae43538d563536677df
anastaciaaaa13 <158077069+anastaciaaaa13@users.noreply.github.com>
2024-12-19 09:39:08 +0300

    Update task3.c

--- 3824Б1ПР4/17 Медведева Анастасия Павловна/task3.c
+++ 3824Б1ПР4/17 Медведева Анастасия Павловна/task3.c
@@ -1,7 +1,25 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить количество слов, 
 // которые начинаются и заканчиваются одной и той же буквой. 
-int task3(char *s)
-{
-  return -1;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int task3(char *s) {
+    int count = 0;
+    char *word;
+
+    while (isspace(*s)) s++;
+
+    word = strtok(s, " ");
+    
+    while (word != NULL) {
+        if (tolower(word[0]) == tolower(word[strlen(word) - 1])) {
+            count++;
+        }
+        
+        word = strtok(NULL, " ");
+    }
+
+    return count;
 }

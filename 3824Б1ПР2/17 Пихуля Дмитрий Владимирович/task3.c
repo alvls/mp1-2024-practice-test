9334a971c9c214ca3c64f4cbd01ccb847dd58dfb
Dimusik52 <124053907+Dimusik52@users.noreply.github.com>
2024-12-19 10:06:07 +0300

    Update task3.c

--- 3824Б1ПР2/17 Пихуля Дмитрий Владимирович/task3.c
+++ 3824Б1ПР2/17 Пихуля Дмитрий Владимирович/task3.c
@@ -1,7 +1,37 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить количество слов, 
 // которые начинаются и заканчиваются одной и той же буквой. 
-int task3(char *s)
+int task3(char* s)
 {
-  return -1;
+	int i = 1;
+
+	int counter = 0;
+
+	char first_letter = '/';
+	char last_letter = ' ';
+	if (s[i - 1] != ' ') {
+		first_letter = s[i - 1];
+	}
+
+	while (s[i] != '\0') {
+		if (s[i] == ' ') {
+			last_letter = s[i - 1];
+			if (first_letter == last_letter) {
+				counter += 1;
+			}
+			first_letter = '/';
+		}
+		if (first_letter == '/' && s[i] != ' ') {
+			first_letter = s[i];
+		}
+		i++;
+	}
+	if (s[i] == '\0') {
+		last_letter = s[i - 1];
+	}
+	if (first_letter == last_letter) {
+		counter += 1;
+	}
+
+	return counter;
 }

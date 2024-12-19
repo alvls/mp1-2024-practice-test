7335770b35de5298c975ef7cbc5d26e066b053db
sofiasheburova <sofyasheburova@gmail.com>
2024-12-19 10:09:58 +0300

    Update task2.c

--- 3824Б1ФИ2/28 Шебурова София Борисовна/task2.c
+++ 3824Б1ФИ2/28 Шебурова София Борисовна/task2.c
@@ -2,7 +2,22 @@
 // Найти значение максимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <stdio.h>
+
+long long task2(unsigned A[], size_t n) { //крайние числа в последовательности не учитываю, как локальные максимумы
+    if (n < 3) {
+        return -1; 
+    }
+
+    long long maxLocalMax = -1;
+
+    for (size_t i = 1; i < n - 1; i++) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+            if (A[i] > maxLocalMax) {
+                maxLocalMax = A[i];
+            }
+        }
+    }
+
+    return maxLocalMax;
 }

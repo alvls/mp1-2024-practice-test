6ef9c3990909ccc8b48e350130b884d9129e586f
efleevS <efleev.sergey0912@gmail.com>
2024-12-19 09:40:12 +0300

    Update task2.c

--- 3824Б1ПР1/07 Ефлеев Сергей Алексеевич/task2.c
+++ 3824Б1ПР1/07 Ефлеев Сергей Алексеевич/task2.c
@@ -2,7 +2,13 @@
 // Найти значение минимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <stdio.h>
+long long task1(unsigned A[], size_t n) {
+  int min_local_min = -1;
+  for (int i = 1; i < n - 1; i++) {
+    if (A[i] <= A[i - 1] && A[i] <= A[i + 1]) {
+      min_local_min = A[i];
+    }
+  }
+  return min_local_min;
 }

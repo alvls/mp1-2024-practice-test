7fab0e7a93873757fbc4aaeaf531d8b09275e790
ryzhil <119815908+ryzhil@users.noreply.github.com>
2024-12-19 09:47:16 +0300

    Update task3.c

--- 3824Б1ПР3/19 Рыжиков Илья Александрович/task3.c
+++ 3824Б1ПР3/19 Рыжиков Илья Александрович/task3.c
@@ -1,7 +1,31 @@
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Скопировать строку s в новую и преобразовать ее так,
 // чтобы все слова в ней начинались с заглавной буквы.
-char* task3(char *s)
-{
-  return "task3";
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>  
+#include <string.h> 
+char* task3(char *s) {
+    size_t len = strlen(s);
+    
+    char *result = (char *)malloc(len + 1);
+
+    int newWord = 1;
+
+    for (size_t i = 0; i < len; i++) {
+        if (s[i] == ' ') {
+            result[i] = ' ';
+            newWord = 1;
+        } else {
+            if (newWord) {
+                result[i] = toupper(s[i]);
+                newWord = 0;
+            } else {
+                result[i] = tolower(s[i]);
+            }
+        }
+    }
+
+    result[len] = '\0';
+    return result;
 }
